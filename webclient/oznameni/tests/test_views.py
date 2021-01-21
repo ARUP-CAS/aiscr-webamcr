20832bcfef8fb0057bf4d7ef69a896b36f0406b6
Juraj Škvarla <juraj.skvarla@huld.io>
2021-01-21 18:08:01 +0100

    adding test for post_poi2kat

--- webclient/oznameni/tests/test_views.py
+++ webclient/oznameni/tests/test_views.py
@@ -1,17 +1,31 @@
+import json
+
 from django.test import RequestFactory, TestCase
-from oznameni.views import index
+from oznameni.views import index, post_poi2kat
 
 
 class UrlTests(TestCase):
     def setUp(self):
-        pass
+        self.factory = RequestFactory()
 
     def test_get_index(self):
-        factory = RequestFactory()
-        request = factory.get("/oznameni/")
+        request = self.factory.get("/oznameni/")
 
         response = index(request)
         self.assertEqual(200, response.status_code)
 
     def test_post_index(self):
+        # TODO add test
         pass
+
+    def test_post_poi2kat(self):
+        josefov = (14.417222, 50.089616)
+        request = self.factory.post(
+            "/oznameni/get-katastr-from-point",
+            {"corY": josefov[0], "corX": josefov[1]},
+            content_type="application/json",
+        )
+        response = post_poi2kat(request)
+        self.assertEqual(200, response.status_code)
+        data = json.loads(response.content)
+        self.assertEqual(data["cadastre"], "JOSEFOV (Praha)")
--- webclient/webclient/urls.py
+++ webclient/webclient/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
-    path("announcement/", include("oznameni.urls")),
+    path("oznameni/", include("oznameni.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
