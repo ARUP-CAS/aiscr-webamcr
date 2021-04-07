64818cd69585c9590de33da0b17d20c58b31fbf1
Petr Kudela <kudy.samba@gmail.com>
2021-04-07 12:53:01 +0200

    form changes

--- webclient/oznameni/forms.py
+++ webclient/oznameni/forms.py
@@ -1,12 +1,11 @@
 import datetime
 import logging
 
-from crispy_forms.bootstrap import FormActions
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV2Invisible
 from core.validators import validate_phone_number
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import HTML, Div, Layout, Submit
+from crispy_forms.layout import HTML, Div, Layout
 from dal import autocomplete
 from django import forms
 from django.utils.translation import gettext as _
