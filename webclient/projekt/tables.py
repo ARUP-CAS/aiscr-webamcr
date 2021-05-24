632b7793346578aae29b293d842e1835a4f0bd26
Najiva <jurajskvarla@yahoo.com>
2021-05-24 12:14:00 +0200

    adding suport for hiding the project table columns

--- webclient/projekt/tables.py
+++ webclient/projekt/tables.py
@@ -1,16 +1,18 @@
 import django_tables2 as tables
 
 from .models import Projekt
+from django_tables2_column_shifter.tables import (
+    ColumnShiftTableBootstrap4,
+)
 
 
-class ProjektTable(tables.Table):
+class ProjektTable(ColumnShiftTableBootstrap4):
 
     ident_cely = tables.Column(linkify=True)
-    stav = tables.Column(attrs={"td": {"class": ".info"}})
 
     class Meta:
         model = Projekt
-        template_name = "projekt/bootstrap4_table.html"
+        template_name = "projekt/bootstrap4.html"
         fields = (
             "ident_cely",
             "stav",
@@ -25,6 +27,10 @@ class ProjektTable(tables.Table):
             "typ_projektu",
             "uzivatelske_oznaceni",
         )
+
+    def __init__(self, *args, **kwargs):
+        super(ProjektTable, self).__init__(*args, **kwargs)
+        # self.set_hideable_columns(['ident_cely', 'stav']) Uncomment when will be supported
 
     @staticmethod
     def render_datum_zahajeni(value):
