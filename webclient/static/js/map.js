322445555a4162073b250607e7264f81d82a4bbc
Jiří Bartoš <jiri.bartos@huld.io>
2021-07-30 16:01:23 +0200

    Fix: map control

--- webclient/static/js/map.js
+++ webclient/static/js/map.js
@@ -26,15 +26,14 @@ var overlays ={
 
 L.control.layers(baseLayers,overlays).addTo(map);
 L.control.scale(metric = "true").addTo(map);
-/*
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(map);*/
+L.control.zoom(
+    {
+        zoomInText:'+',
+        zoomInTitle:'Zvětšit',
+        zoomOutText:'-',
+        zoomOutTitle:'Zmenšit'
+    }).addTo(map)
+
 var poi = L.layerGroup();
 map.addLayer(poi);
 
