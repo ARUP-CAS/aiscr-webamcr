367da902884cf3f505f75e57035845ae229f5788
jiri.pesik <jiri.pesik@outlook.cz>
2021-09-07 20:14:30 +0200

    Ignore click #101

--- webclient/arch_z/forms.py
+++ webclient/arch_z/forms.py
@@ -50,8 +50,10 @@ class CreateArchZForm(forms.ModelForm):
         super(CreateArchZForm, self).__init__(*args, **kwargs)
         self.fields["katastry"].required = False
         self.fields["hlavni_katastr"].required = False
-        self.fields["katastry"].disabled = True
-        self.fields["hlavni_katastr"].disabled = True
+        self.fields["katastry"].widget.attrs['readonly'] = True
+        self.fields["katastry"].widget.attrs['style'] = "pointer-events: none;"
+        self.fields["hlavni_katastr"].widget.attrs['readonly'] = True
+        self.fields["hlavni_katastr"].widget.attrs['style'] = "pointer-events: none;"
         if projekt:
             self.fields["hlavni_katastr"].initial = projekt.hlavni_katastr
             self.fields["uzivatelske_oznaceni"].initial = projekt.uzivatelske_oznaceni
@@ -61,9 +63,9 @@ class CreateArchZForm(forms.ModelForm):
 
         self.helper.layout = Layout(
             Div(
-                Div("hlavni_katastr", css_class="col-sm-4"),
+                Div("hlavni_katastr", css_class="col-sm-4", style="pointer-events: none;"),
                 Div("pristupnost", css_class="col-sm-4"),
-                Div("katastry", css_class="col-sm-4"),
+                Div("katastry", css_class="col-sm-4", style="pointer-events: none;"),
                 Div("uzivatelske_oznaceni", css_class="col-sm-12"),
                 css_class="row",
             ),
