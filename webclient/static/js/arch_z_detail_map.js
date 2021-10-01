32552c63ac784d0ac29ae26524394547501423da
Jiří Bartoš <jiri.bartos@huld.io>
2021-10-01 20:15:10 +0200

    Fix: dj icons divided sqrt(2)

--- webclient/static/js/arch_z_detail_map.js
+++ webclient/static/js/arch_z_detail_map.js
@@ -5,37 +5,37 @@ var global_map_can_grab_geom_from_map=false;
 var blueIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconSize: [18, 29],
+    iconAnchor: [9, 29],
+    popupAnchor: [1, -24],
+    shadowSize: [29, 29]
     })
 
 var redIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconSize: [18, 29],
+    iconAnchor: [9, 29],
+    popupAnchor: [1, -24],
+    shadowSize: [29, 29]
     })
 
 var greenIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconSize: [18, 29],
+    iconAnchor: [9, 29],
+    popupAnchor: [1, -24],
+    shadowSize: [29, 29]
     })
 
 var goldIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    iconSize: [18, 29],
+    iconAnchor: [9, 29],
+    popupAnchor: [1, -24],
+    shadowSize: [29, 29]
     })
 
     var osmColor = L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: 'OSM map', maxZoom:25, maxNativeZoom: 19, minZoom: 6 }),
