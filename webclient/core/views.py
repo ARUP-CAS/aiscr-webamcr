268e3631ee76706a6c7e910243298ce9a3f8b8d6
Jan Ihnat <jnihnat@gmail.com>
2022-01-20 20:05:58 +0100

    uprava nazvu suboru pri stahovani

--- webclient/core/views.py
+++ webclient/core/views.py
@@ -79,8 +79,8 @@ def download_file(request, pk):
         ]  # Use mimetypes to get file type
         response = HttpResponse(soubor.path, content_type=content_type)
         response["Content-Length"] = str(len(soubor.path))
-        response["Content-Disposition"] = "inline; filename=" + os.path.basename(
-            soubor.nazev_zkraceny
+        response["Content-Disposition"] = (
+            "attachment; filename=" + soubor.nazev_zkraceny
         )
         return response
     else:
