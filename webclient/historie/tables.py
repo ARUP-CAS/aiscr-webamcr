cfdc01b03965b96587d9dcbbf909cbe3c36a5744
jnihnat <57906106+jnihnat@users.noreply.github.com>
2022-05-05 09:03:25 +0200

    upravena tabulka s historii. Upravena tabulka s projektem (#449)

--- webclient/historie/tables.py
+++ webclient/historie/tables.py
@@ -1,12 +1,19 @@
 from django_tables2 import tables, columns
 from historie.models import Historie
+from django_tables2_column_shifter.tables import (
+    ColumnShiftTableBootstrap4,
+)
 
 
-class HistorieTable(tables.Table):
-    datum_zmeny = columns.DateTimeColumn(format ='Y-m-d, H:i')
+class HistorieTable(ColumnShiftTableBootstrap4):
+    datum_zmeny = columns.DateTimeColumn(format ='Y-m-d, H:i',default="")
+    typ_zmeny = columns.Column(default="")
+    poznamka = columns.Column(default="")
+    uzivatel = columns.Column(default="")
     class Meta:
         model = Historie
-        template_name = "django_tables2/bootstrap4.html"
+        template_name = "projekt/bootstrap4.html"
+        # template_name = "django_tables2/bootstrap4.html"
         fields = (
             "typ_zmeny",
             "datum_zmeny",
