23c6441219eddca7f26fad13593dfb06aa1c997a
pesikj <jiri.pesik@outlook.cz>
2022-05-17 22:13:06 +0200

    Úpravy dalších formulářů

--- webclient/komponenta/views.py
+++ webclient/komponenta/views.py
@@ -70,73 +70,53 @@ def detail(request, ident_cely):
         logger.debug(form.errors)
         messages.add_message(request, messages.ERROR, ZAZNAM_SE_NEPOVEDLO_EDITOVAT)
 
-    druh_objekt_choices = heslar_12(HESLAR_OBJEKT_DRUH, HESLAR_OBJEKT_DRUH_KAT)
-    specifikace_objekt_choices = heslar_12(
-        HESLAR_OBJEKT_SPECIFIKACE, HESLAR_OBJEKT_SPECIFIKACE_KAT
-    )
-    NalezObjektFormset = inlineformset_factory(
-        Komponenta,
-        NalezObjekt,
-        form=create_nalez_objekt_form(druh_objekt_choices, specifikace_objekt_choices),
-        extra=1,
-    )
-    formset_objekt = NalezObjektFormset(
-        request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_o"
-    )
-
-    druh_predmet_choices = heslar_12(HESLAR_PREDMET_DRUH, HESLAR_PREDMET_DRUH_KAT)
-    specifikce_predmetu_choices = list(
-        Heslar.objects.filter(nazev_heslare=HESLAR_PREDMET_SPECIFIKACE).values_list(
-            "id", "heslo"
+    if "nalez_edit_nalez" in request.POST:
+        druh_objekt_choices = heslar_12(HESLAR_OBJEKT_DRUH, HESLAR_OBJEKT_DRUH_KAT)
+        specifikace_objekt_choices = heslar_12(
+            HESLAR_OBJEKT_SPECIFIKACE, HESLAR_OBJEKT_SPECIFIKACE_KAT
         )
-    )
-    NalezPredmetFormset = inlineformset_factory(
-        Komponenta,
-        NalezPredmet,
-        form=create_nalez_predmet_form(
-            druh_predmet_choices, specifikce_predmetu_choices
-        ),
-        extra=1,
-    )
-    formset_predmet = NalezPredmetFormset(
-        request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_p"
-    )
-    if formset_objekt.is_valid() and formset_predmet.is_valid():
-        logger.debug("Form is valid")
-        formset_predmet.save()
-        formset_objekt.save()
-        if formset_objekt.has_changed() or formset_predmet.has_changed():
-            logger.debug("Form data was changed")
-            messages.add_message(request, messages.SUCCESS, ZAZNAM_USPESNE_EDITOVAN)
-    else:
-        logger.warning("Form is not valid")
-        logger.debug(formset_predmet.errors)
-        logger.debug(formset_objekt.errors)
-        messages.add_message(request, messages.ERROR, ZAZNAM_SE_NEPOVEDLO_EDITOVAT)
-        request.session["_old_nalez_post"] = request.POST
-        request.session["komp_ident_cely"] = ident_cely
+        NalezObjektFormset = inlineformset_factory(
+            Komponenta,
+            NalezObjekt,
+            form=create_nalez_objekt_form(druh_objekt_choices, specifikace_objekt_choices),
+            extra=1,
+        )
+        formset_objekt = NalezObjektFormset(
+            request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_o"
+        )
 
-    dj_ident_cely = 0
-    dj = get_object_or_404(DokumentacniJednotka, ident_cely=dj_ident_cely)
-    form = CreateADBForm(request.POST)
-    if form.is_valid():
-        logger.debug("Form is valid")
-        adb = form.save(commit=False)
-        if not dj.pian:
-            raise DJNemaPianError(dj)
-        try:
-            adb.ident_cely, sm5 = get_adb_ident(dj.pian)
-        except MaximalIdentNumberError as e:
-            messages.add_message(request, messages.ERROR, e.message)
+        druh_predmet_choices = heslar_12(HESLAR_PREDMET_DRUH, HESLAR_PREDMET_DRUH_KAT)
+        specifikce_predmetu_choices = list(
+            Heslar.objects.filter(nazev_heslare=HESLAR_PREDMET_SPECIFIKACE).values_list(
+                "id", "heslo"
+            )
+        )
+        NalezPredmetFormset = inlineformset_factory(
+            Komponenta,
+            NalezPredmet,
+            form=create_nalez_predmet_form(
+                druh_predmet_choices, specifikce_predmetu_choices
+            ),
+            extra=1,
+        )
+        formset_predmet = NalezPredmetFormset(
+            request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_p"
+        )
+        if formset_objekt.is_valid() and formset_predmet.is_valid():
+            logger.debug("Form is valid")
+            formset_predmet.save()
+            formset_objekt.save()
+            if formset_objekt.has_changed() or formset_predmet.has_changed():
+                logger.debug("Form data was changed")
+                messages.add_message(request, messages.SUCCESS, ZAZNAM_USPESNE_EDITOVAN)
         else:
-            adb.dokumentacni_jednotka = dj
-            adb.sm5 = sm5
-            adb.save()
-            messages.add_message(request, messages.SUCCESS, ZAZNAM_USPESNE_VYTVOREN)
-    else:
-        logger.warning("Form is not valid")
-        logger.debug(form.errors)
-        messages.add_message(request, messages.ERROR, ZAZNAM_SE_NEPOVEDLO_VYTVORIT)
+            logger.warning("Form is not valid")
+            logger.debug(formset_predmet.errors)
+            logger.debug(formset_objekt.errors)
+            messages.add_message(request, messages.ERROR, ZAZNAM_SE_NEPOVEDLO_EDITOVAT)
+            request.session["_old_nalez_post"] = request.POST
+            request.session["komp_ident_cely"] = ident_cely
+
 
 
     response = redirect(request.META.get("HTTP_REFERER"))
