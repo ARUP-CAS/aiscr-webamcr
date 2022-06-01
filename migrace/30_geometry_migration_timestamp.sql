922b81d250cfc87e2dc737b2ff15afa5a0902b07
jiri.pesik <jiri.pesik@outlook.cz>
2022-06-01 17:28:47 +0200

    Syntax fix

--- migrace/30_geometry_migration_timestamp.sql
+++ migrace/30_geometry_migration_timestamp.sql
@@ -12,14 +12,14 @@ CREATE TABLE public.amcr_geom_migrations_jobs(
     count_error_sjtsk integer default 0 not null,
     created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     detail text default null
-)
+);
 
 CREATE TABLE public.amcr_geom_migrations_jobs_wgs84_errors(
     id serial primary key,
     pian_id serial
-)
+);
 
 CREATE TABLE public.amcr_geom_migrations_jobs_sjtsk_errors(
     id serial primary key,
     pian_id serial
-)
+);
