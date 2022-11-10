4c5a19a1798d139cd41468367f52e0a58e604157
David Novák <novak@arup.cas.cz>
2022-11-10 20:32:13 +0100

    preklad EN organizace

--- migrace/41_DN_ruzne_opravy.sql
+++ migrace/41_DN_ruzne_opravy.sql
@@ -1,4 +1,5 @@
--- Placeholdery pro chybějící či duplkicitní EN hesla.
+-- Řešení pro chybějící či duplkicitní EN hesla.
 UPDATE heslar SET heslo_en = 'translate: ' & ident_cely WHERE (heslo_en Is Null);
 UPDATE heslar SET heslo_en = 'chain necklace' WHERE ident_cely = 'HES-000756';
 UPDATE heslar SET heslo_en = 'chopper' WHERE ident_cely = 'HES-000801';
+UPDATE organizace SET nazev_zkraceny_en = 'translate: ' & id WHERE (nazev_zkraceny_en Is Null);
