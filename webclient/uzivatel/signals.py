fa66adf1b9136512136a17b4fd61b80de5686f16
Jiří Pešík <jiri.pesik@outlook.cz>
2022-11-21 17:33:45 +0100

    Automatic tests fix #389

--- webclient/uzivatel/signals.py
+++ webclient/uzivatel/signals.py
@@ -1,5 +1,7 @@
 import logging
 
+import structlog
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
@@ -7,14 +9,17 @@ from services.mailer import Mailer
 from uzivatel.models import User
 
 logger = logging.getLogger(__name__)
-
+logger_s = structlog.get_logger(__name__)
 
 @receiver(pre_save, sender=User)
 def create_ident_cely(sender, instance, **kwargs):
     # check if the updated fields exist and if you're not creating a new object
     if not kwargs['update_fields'] and instance.id:
         # Save it, so it can be used in post_save
-        instance.old = User.objects.get(id=instance.id)
+        try:
+            instance.old = User.objects.get(id=instance.id)
+        except ObjectDoesNotExist as err:
+            logger_s.error("signals.create_ident_cely.err", err=err)
     if instance.pk is None:
         instance.model_is_updated = False
         logger.debug("Running create_ident_cely receiver ...")
@@ -33,20 +38,23 @@ def create_ident_cely(sender, instance, **kwargs):
 def send_deactivation_email(sender, instance: User, **kwargs):
     if not kwargs.get('update_fields') and hasattr(instance, 'old'):
         kwargs['update_fields'] = []
-        if (instance.is_active != instance.old.is_active):
+        if instance.is_active != instance.old.is_active:
             kwargs['update_fields'].append('is_active')
-    if (kwargs['update_fields']):
+    if kwargs['update_fields']:
         if 'is_active' in kwargs['update_fields'] and instance.is_active == False:
             Mailer.sendEU03(user=instance)
 
 
 @receiver(post_save, sender=User)
 def send_new_user_email_to_admin(sender, instance: User, **kwargs):
-    if (kwargs.get('created') == True) and instance.created_from_admin_panel == False:
-        Mailer.sendEU04(user=instance)
+    if (kwargs.get('created') is True) and instance.created_from_admin_panel is False:
+        try:
+            Mailer.sendEU04(user=instance)
+        except ObjectDoesNotExist as err:
+            logger_s.error("signals.send_new_user_email_to_admin.err", err=err)
 
 
 @receiver(post_save, sender=User)
 def send_account_confirmed_email(sender, instance: User, **kwargs):
-    if kwargs.get('created') == True and instance.created_from_admin_panel == True:
+    if kwargs.get('created') is True and instance.created_from_admin_panel is True:
         Mailer.sendEU02(user=instance)
