d7f87669e8b8ebca81e5b1a09d4c5c7204492ec6
Marek Sedlacek <sedlacek.marek@gmail.com>
2022-12-16 23:42:23 +0100

    Fix bug for translations (data not persistent) in entrypoint.sh

---
+++ scripts/build_prod_image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+#Wrapper for building production image locally
+docker build -t test_prod -f Dockerfile-production --build-arg VERSION_APP="$(git rev-parse --short HEAD | head -c 8)" --build-arg TAG_APP=local_build  .
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -7,20 +7,34 @@ export DJANGO_SETTINGS_MODULE=webclient.settings.production
 python3 manage.py collectstatic --noinput
 
 #Copy locale from volume, create new one/update old, copy locale to volume and remove from app. Move has permission denied.
-test -e /vol/web/locale/cs/LC_MESSAGES/django.po && cp /vol/web/locale/cs/LC_MESSAGES/django.po /code/locale/cs/LC_MESSAGES/django.po || echo "CS locale file does not exist will create new one"
-test -e /vol/web/locale/en/LC_MESSAGES/django.po && cp /vol/web/locale/en/LC_MESSAGES/django.po /code/locale/en/LC_MESSAGES/django.po || echo "EN locale file does not exist will create new one"
 
-mkdir -p locale/cs/LC_MESSAGES
-mkdir -p locale/en/LC_MESSAGES
+languages=( "cs" "en" )
 
-python3 manage.py makemessages -l cs
-python3 manage.py makemessages -l en
+#Source of locale on persistent volume
+volume_locale="/vol/web/locale/"
+locale_file="LC_MESSAGES/django.po"
+locale_cp_root="/code/locale"
 
-cp /code/locale/cs/LC_MESSAGES/django.po /vol/web/locale/cs/LC_MESSAGES/django.po
-cp /code/locale/en/LC_MESSAGES/django.po /vol/web/locale/en/LC_MESSAGES/django.po
+for lang_item in ${languages[@]}; do
+  
+  vol_locale="${volume_locale}/${lang_item}/${locale_file}"
+  cp_path_locale="${locale_cp_root}/${lang_item}/${locale_file}"
+  
+  echo "#make dirs $(dirname ${cp_path_locale})"
+  mkdir -p $(dirname ${cp_path_locale})
+  
+  test -e ${vol_locale} && cp ${vol_locale} ${cp_path_locale} || echo "${lang_item} locale file does not exist will create new one"
+  
+  echo "#makemessages ${lang_item}"
+  python3 manage.py makemessages -l ${lang_item}
+  
+  echo "#copy ${lang_item} from tmp path back to persitent volume location"
+  cp ${cp_path_locale} ${vol_locale}
 
-rm /code/locale/cs/LC_MESSAGES/django.po
-rm /code/locale/en/LC_MESSAGES/django.po
+  echo "#remove tmp locale ${lang_item}"
+  rm ${cp_path_locale}
+
+done
 
 uwsgi --socket :8000 \
   --master --enable-threads --module webclient.wsgi
