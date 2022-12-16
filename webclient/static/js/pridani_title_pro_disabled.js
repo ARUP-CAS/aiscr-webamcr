e79c4f5ad2a9dd6a469a1ee753b0942564c4d1c2
Jiří Pešík <jiri.pesik@outlook.cz>
2022-12-16 14:29:03 +0100

    Bugfix #269

--- webclient/static/js/pridani_title_pro_disabled.js
+++ webclient/static/js/pridani_title_pro_disabled.js
@@ -1,13 +1,13 @@
 window.onload = function () {
     inputs = document.querySelectorAll("input[type=text]");
     for (index = 0; index < inputs.length; ++index) {
-        if (inputs[index].readonly === "readonly") {
+        if (inputs[index].readOnly === true) {
             inputs[index].title = inputs[index].value
         }
     }
     textareas = document.querySelectorAll("textarea");
     for (index = 0; index < textareas.length; ++index) {
-        if (inputs[index].readonly === "readonly") {
+        if (inputs[index].readOnly === true) {
             textareas[index].title = textareas[index].value
         }
     }
