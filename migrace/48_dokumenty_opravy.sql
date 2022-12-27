f017e902655d06af8fb54e9517becf31ec2ec12e
David Novák <novak@arup.cas.cz>
2022-12-27 13:55:49 +0100

    Oprava formatovani

--- migrace/48_dokumenty_opravy.sql
+++ migrace/48_dokumenty_opravy.sql
@@ -1,47 +1,42 @@
 -- Odstraneni souboru bez vazby
-with navic_soubory as (
-        select distinct(sv.id) as soubor_id,
-                sv.typ_vazby,
-                d.id
-        from soubor s
-                join soubor_vazby sv on s.vazba = sv.id
-                left join dokument d on d.soubory = sv.id
-        where d.id is null
-                and sv.typ_vazby = 'dokument'
-)
-delete from soubor s2
-where s2.vazba in (
-                select soubor_id
-                from navic_soubory
-        ) with navic_soubory as (
-                select distinct(sv.id) as soubor_id,
-                        sv.typ_vazby,
-                        d.id
-                from soubor s
-                        join soubor_vazby sv on s.vazba = sv.id
-                        left join projekt d on d.soubory = sv.id
-                where d.id is null
-                        and sv.typ_vazby = 'projekt'
-        )
-delete from soubor s2
-where s2.vazba in (
-                select soubor_id
-                from navic_soubory
-        ) with navic_soubory as (
-                select distinct(sv.id) as soubor_id,
-                        sv.typ_vazby,
-                        sn.id
-                from soubor s
-                        join soubor_vazby sv on s.vazba = sv.id
-                        left join samostatny_nalez sn on sn.soubory = sv.id
-                where sn.id is null
-                        and sv.typ_vazby = 'samostatny_nalez'
-        )
-delete from soubor s2
-where s2.vazba in (
-                select soubor_id
-                from navic_soubory
-        );
+WITH navic_soubory AS
+  (SELECT distinct(sv.id) AS soubor_id,
+          sv.typ_vazby,
+          d.id
+   FROM soubor s
+   JOIN soubor_vazby sv ON s.vazba = sv.id
+   LEFT JOIN dokument d ON d.soubory = sv.id
+   WHERE d.id IS NULL
+     AND sv.typ_vazby = 'dokument' )
+DELETE
+FROM soubor s2
+WHERE s2.vazba in
+    (SELECT soubor_id
+     FROM navic_soubory) WITH navic_soubory AS
+    (SELECT distinct(sv.id) AS soubor_id,
+            sv.typ_vazby,
+            d.id
+     FROM soubor s
+     JOIN soubor_vazby sv ON s.vazba = sv.id
+     LEFT JOIN projekt d ON d.soubory = sv.id
+     WHERE d.id IS NULL
+       AND sv.typ_vazby = 'projekt' )
+  DELETE
+  FROM soubor s2 WHERE s2.vazba in
+    (SELECT soubor_id
+     FROM navic_soubory) WITH navic_soubory AS
+    (SELECT distinct(sv.id) AS soubor_id,
+            sv.typ_vazby,
+            sn.id
+     FROM soubor s
+     JOIN soubor_vazby sv ON s.vazba = sv.id
+     LEFT JOIN samostatny_nalez sn ON sn.soubory = sv.id
+     WHERE sn.id IS NULL
+       AND sv.typ_vazby = 'samostatny_nalez' )
+  DELETE
+  FROM soubor s2 WHERE s2.vazba in
+    (SELECT soubor_id
+     FROM navic_soubory);
         
 --update razeni pri pristupnost heslare
 update heslar h
