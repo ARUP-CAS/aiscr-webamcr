5bee46e268c6a218fc4444cd7f10ecf1a75caa56
David Novák <novak@arup.cas.cz>
2022-12-28 23:52:19 +0100

    Optimalizace pomalých dotazů

--- migrace/6_migrace_dalsi.sql
+++ migrace/6_migrace_dalsi.sql
@@ -87,9 +87,11 @@ update historie_vazby set typ_vazby='archeologicky_zaznam' where typ_vazby='akce
 
 -- Jeste jsem zapomel domigrovat tranzakce akci 3 a 7 na 4
 -- TODO Issue 107
-update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=111);
--- COMMENT: tahle query je nejaka pomala, nevim proc ... :(
-update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=7);
+-- update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=111);
+UPDATE historie h SET typ_zmeny = 4 WHERE id IN (SELECT his.id AS hid FROM historie his WHERE his.vazba IN (SELECT hv.id FROM historie_vazby hv WHERE hv.typ_vazby='archeologicky_zaznam') AND his.typ_zmeny=111);
+-- COMMENT: tahle query je nejaka pomala, nevim proc ... :( DN: nahrazeno alternativou;
+-- update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=7);
+UPDATE historie h SET typ_zmeny = 4 WHERE id IN (SELECT his.id AS hid FROM historie his WHERE his.vazba IN (SELECT hv.id FROM historie_vazby hv WHERE hv.typ_vazby='archeologicky_zaznam') AND his.typ_zmeny=7);
 
 -- Odstraneni tranzakce AKTUALIZACE z historie samostatnych nalezu
 delete from historie where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='samostatny_nalez' and his.typ_zmeny = 8);
