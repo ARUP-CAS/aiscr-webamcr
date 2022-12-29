baab8452948729d12c68a2a489bfb34bcf99a51f
David Novák <novak@arup.cas.cz>
2022-12-29 16:48:25 +0100

    Nastaveni editace heslaru

--- migrace/44_DN_ruzne_opravy.sql
+++ migrace/44_DN_ruzne_opravy.sql
@@ -22,3 +22,57 @@ DELETE FROM oznamovatel WHERE email = 'údaj odstraněn' AND adresa = 'údaj ods
 -- Odstranění nepotřebných tabulek
 DROP TABLE projekt_oznameni_suffix;
 DROP TABLE systemove_promenne;
+
+-- Odstranění nepotřebných heslářů
+DELETE FROM heslar WHERE nazev_heslare = (SELECT id FROM heslar_nazev WHERE nazev = 'autorska_role');
+DELETE FROM heslar_nazev WHERE nazev = 'autorska_role';
+
+-- Nastavení editovatelnosti heslářů
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'adb_podnet';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'adb_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'akce_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'akce_typ_kat';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'aktivita';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'areal';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'areal_kat';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'datum_specifikace';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'dohlednost';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'dok_jednotka_typ';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'dokument_format';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'dokument_material';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'dokument_nahrada';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'dokument_rada';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'dokument_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'dokument_ulozeni';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'dokument_zachovalost';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'ext_zdroj_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'jazyk';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'jistota_urceni';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'letfoto_tvar';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'letiste';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'lokalita_druh';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'lokalita_druh_kat';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'lokalita_typ';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'nalez_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'nalezove_okolnosti';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'obdobi';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'obdobi_kat';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'objekt_druh';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'objekt_druh_kat';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'objekt_specifikace';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'objekt_specifikace_kat';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'organizace_typ';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'pamatkova_ochrana';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'pian_presnost';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'pian_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'pocasi';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'posudek_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'predmet_druh';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'predmet_druh_kat';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'predmet_specifikace';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'pristupnost';
+UPDATE heslar_nazev SET povolit_zmeny = false WHERE nazev = 'projekt_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'stav_dochovani';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'udalost_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'vyskovy_bod_typ';
+UPDATE heslar_nazev SET povolit_zmeny = true WHERE nazev = 'zeme';
