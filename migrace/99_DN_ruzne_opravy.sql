aba622e0e55cbd6c4ac2005caf666e04e7f7136f
David Novák <novak@arup.cas.cz>
2022-12-29 13:40:20 +0100

    Uprava migrace dokumentu
    - Odstranění dokumentů ZA/ZL
    - Migrace soubor.vlastnik a soubor.vytvoreno do historie

--- migrace/99_DN_ruzne_opravy.sql
+++ migrace/99_DN_ruzne_opravy.sql
@@ -3,3 +3,15 @@ UPDATE heslar SET heslo_en = 'translate: ' || ident_cely WHERE (heslo_en Is Null
 UPDATE heslar SET heslo_en = 'chain necklace' WHERE ident_cely = 'HES-000756';
 UPDATE heslar SET heslo_en = 'chopper' WHERE ident_cely = 'HES-000801';
 UPDATE organizace SET nazev_zkraceny_en = 'translate: ' || id WHERE (nazev_zkraceny_en Is Null);
+
+-- Odstranění dokumentů ZA/ZL
+DELETE FROM dokument, soubor USING dokument
+INNER JOIN heslar ON dokument.rada = heslar.id
+INNER JOIN soubor ON soubor.vazba = dokument.soubory
+WHERE heslar.ident_cely = 'HES-000884' OR heslar.ident_cely = 'HES-000885';
+
+-- Migrace soubor.vlastnik a soubor.vytvoreno do historie
+UPDATE soubor SET historie = (INSERT INTO historie_vazby (typ_vazby) VALUES 'soubor' RETURNING id) WHERE soubor.historie IS NULL;
+ALTER TABLE soubor ADD CONSTRAINT soubor_historie_key UNIQUE (historie);
+INSERT INTO historie (datum_zmeny, uzivatel, poznamka, vazba, typ_zmeny) SELECT vytvoreno, vlastnik, nazev_puvodni, historie, 'SBR0' FROM soubor;
+ALTER TABLE soubor DROP COLUMN vytvoreno, vlastnik;
