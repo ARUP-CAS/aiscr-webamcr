8803e8a56ff49e4e3d368d66462d6e67544129b6
Jiří Pešík <jiri.pesik@outlook.cz>
2023-01-24 22:32:10 +0100

    Refactor of slow queries

--- migrace/6b1_migrace_dalsi.sql
+++ migrace/6b1_migrace_dalsi.sql
@@ -2,4 +2,8 @@
 -- Jeste jsem zapomel domigrovat tranzakce akci 3 a 7 na 4
 -- TODO Issue 107
 -- update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=111);
-UPDATE historie h SET typ_zmeny = 4 WHERE id IN (SELECT his.id AS hid FROM historie his WHERE his.vazba IN (SELECT hv.id FROM historie_vazby hv WHERE hv.typ_vazby='archeologicky_zaznam') AND his.typ_zmeny=111);
+UPDATE historie h SET typ_zmeny = 4
+FROM historie_vazby hv
+WHERE hv.typ_vazby='archeologicky_zaznam'
+AND h.vazba = hv.id
+AND hv.typ_zmeny=111;
--- migrace/6b2_migrace_dalsi.sql
+++ migrace/6b2_migrace_dalsi.sql
@@ -1,3 +1,7 @@
 -- COMMENT: tahle query je nejaka pomala, nevim proc ... :( DN: nahrazeno alternativou;
 -- update historie h set typ_zmeny = 4 where id in (select his.id as hid from historie his join historie_vazby as hv on hv.id=his.vazba where hv.typ_vazby='archeologicky_zaznam' and his.typ_zmeny=7);
-UPDATE historie h SET typ_zmeny = 4 WHERE id IN (SELECT his.id AS hid FROM historie his WHERE his.vazba IN (SELECT hv.id FROM historie_vazby hv WHERE hv.typ_vazby='archeologicky_zaznam') AND his.typ_zmeny=7);
+UPDATE historie h SET typ_zmeny = 4
+FROM historie_vazby hv
+WHERE hv.typ_vazby='archeologicky_zaznam'
+AND h.vazba = hv.id
+AND hv.typ_zmeny = 7;
