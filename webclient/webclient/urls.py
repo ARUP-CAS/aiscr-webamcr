6c897273d9159acab2ae484553f55d99b2ef8900
Marek Sedlacek <sedlacek.marek@gmail.com>
2023-04-24 00:48:00 +0200

    Adding healthcheck to the application

--- webclient/webclient/urls.py
+++ webclient/webclient/urls.py
@@ -72,6 +72,8 @@ urlpatterns = [
     path("notifikace_projekty/",  include("notifikace_projekty.urls")),
     # path("select2/", include("django_select2.urls"))
 ]
+urlpatterns+=[re_path(r'^healthcheck/', include("healthcheck.urls"))]
+
 if "rosetta" in settings.INSTALLED_APPS:
     urlpatterns += [re_path(r"^rosetta/", include("rosetta.urls"))]
 
