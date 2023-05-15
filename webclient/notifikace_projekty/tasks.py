de492dc965df70b70bfeae4185e9715c545e9e9f
Jiří Pešík <jiri.pesik@outlook.cz>
2023-05-15 18:46:37 +0200

    Task converted to celery #912 #389 (#968)

--- webclient/notifikace_projekty/tasks.py
+++ webclient/notifikace_projekty/tasks.py
@@ -8,7 +8,7 @@ from .models import Pes
 from django.contrib.contenttypes.models import ContentType
 from heslar.models import RuianKraj, RuianOkres, RuianKatastr
 
-logger_s = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -16,7 +16,7 @@ def check_hlidaci_pes(projekt_id):
     """
     Task pro celery pro skontrolování jestli je nastavený hlídací pes.
     """
-    logger_s.debug("cron.Notifications.collect_watchdogs.start")
+    logger.debug("cron.Notifications.collect_watchdogs.start")
     projekt = Projekt.objects.get(pk=projekt_id)
     users_to_notify = Pes.objects.none()
     users_to_notify |= Pes.objects.filter(
@@ -31,6 +31,6 @@ def check_hlidaci_pes(projekt_id):
         content_type=ContentType.objects.get_for_model(RuianKatastr),
         object_id=projekt.hlavni_katastr.id,
     )
-    logger_s.debug("cron.Notifications.collect_watchdogs.watchdog_list", extra={"users_to_notify": users_to_notify})
-    logger_s.debug("cron.Notifications.collect_watchdogs.end")
-    return Mailer.send_ep02(users_to_notify.distinct("user"),projekt)
+    logger.debug("cron.Notifications.collect_watchdogs.watchdog_list", extra={"users_to_notify": users_to_notify})
+    logger.debug("cron.Notifications.collect_watchdogs.end")
+    return Mailer.send_ep02(users_to_notify.distinct("user"), projekt)
