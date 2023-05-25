c175129f12b8a7adcfab44b7bbb1d67fd39684fd
David Novák <novak@arup.cas.cz>
2023-05-25 17:48:28 +0200

    Oprava nastavení vazeb na hesla (#986)
    * Oprava nastavení vazeb na hesla
    
    Souvisí s #841
    
    * Oprava syntaxe

--- webclient/heslar/hesla.py
+++ webclient/heslar/hesla.py
@@ -1,44 +1,44 @@
 from typing import Final
 
 # Pouzite heslare v kodu
-TYP_PROJEKTU_ZACHRANNY_ID: Final = 1135  # HES-001136
-TYP_PROJEKTU_PRUZKUM_ID: Final = 1136  # HES-001138
-TYP_PROJEKTU_BADATELSKY_ID: Final = 1134  # HES-001137
+TYP_PROJEKTU_ZACHRANNY_ID: Final = Heslar.objects.get(ident_cely="HES-001136").pk
+TYP_PROJEKTU_PRUZKUM_ID: Final = Heslar.objects.get(ident_cely="HES-001138").pk
+TYP_PROJEKTU_BADATELSKY_ID: Final = Heslar.objects.get(ident_cely="HES-001137").pk
 
-PRISTUPNOST_BADATEL_ID: Final = 864  # HES-000866
-PRISTUPNOST_ARCHEOLOG_ID: Final = 865  # HES-000867
-PRISTUPNOST_ANONYM_ID: Final = 863  # HES-000865
-PRISTUPNOST_ARCHIVAR_ID: Final = 866  # HES-000868
+PRISTUPNOST_BADATEL_ID: Final = Heslar.objects.get(ident_cely="HES-000866").pk
+PRISTUPNOST_ARCHEOLOG_ID: Final = Heslar.objects.get(ident_cely="HES-000867").pk
+PRISTUPNOST_ANONYM_ID: Final = Heslar.objects.get(ident_cely="HES-000865").pk
+PRISTUPNOST_ARCHIVAR_ID: Final = Heslar.objects.get(ident_cely="HES-000868").pk
 
-SPECIFIKACE_DATA_PRESNE: Final = 885  # HES-000887
-TYP_DJ_SONDA_ID: Final = 1070  # HES-001072
-TYP_DJ_KATASTR: Final = 1071 # HES-001073
-TYP_DJ_CELEK: Final = 1068 # HES-001070
-TYP_DJ_CAST: Final = 1069 # HES-001071
-TYP_DJ_LOKALITA: Final = 1072 # HES-001074
+SPECIFIKACE_DATA_PRESNE: Final = Heslar.objects.get(ident_cely="HES-000887").pk
+TYP_DJ_SONDA_ID: Final = Heslar.objects.get(ident_cely="HES-001072").pk
+TYP_DJ_KATASTR: Final = Heslar.objects.get(ident_cely="HES-001073").pk
+TYP_DJ_CELEK: Final = Heslar.objects.get(ident_cely="HES-001070").pk
+TYP_DJ_CAST: Final = Heslar.objects.get(ident_cely="HES-001071").pk
+TYP_DJ_LOKALITA: Final = Heslar.objects.get(ident_cely="HES-001074").pk
 
-GEOMETRY_PLOCHA: Final = 1133  # HES-001135
-GEOMETRY_LINIE: Final = 1132  # HES-001134
-GEOMETRY_BOD: Final = 1131  # HES-001133
+GEOMETRY_PLOCHA: Final = Heslar.objects.get(ident_cely="HES-001135").pk
+GEOMETRY_LINIE: Final = Heslar.objects.get(ident_cely="HES-001134").pk
+GEOMETRY_BOD: Final = Heslar.objects.get(ident_cely="HES-001133").pk
 
 # Rada modelu 3D
-DOKUMENT_RADA_DATA_3D: Final = 883  # HES-000870
-MATERIAL_DOKUMENTU_DIGITALNI_SOUBOR: Final = 210  # HES-000217
+DOKUMENT_RADA_DATA_3D: Final = Heslar.objects.get(ident_cely="HES-000870").pk
+MATERIAL_DOKUMENTU_DIGITALNI_SOUBOR: Final = Heslar.objects.get(ident_cely="HES-000217").pk
 
 # Typy dokumentu povolene pridat k akci/projektu
-DATA_GPS = 1083  # HES-001085
-EXPERTNI_POSUDEK = 1075  # HES-001077
-HLASENI_ZAA = 1078  # HES-001080
-TYP_DOKUMENTU_NALEZOVA_ZPRAVA: Final = 1073  # HES-001075
-DATA_ANALYZY_EKOFAKTORU = 1080  # HES-001082
-INVESTORSKA_ZPRAVA = 1077  # HES-001079
-RESTAURATORSKA_ZPRAVA = 1086  # HES-001078
-DATA_VEKTOROVEHO_PLANU = 1085  # HES-001087
-TEXT_JINY = 1079  # HES-001081
-DATA_TERENNIHO_VYZKUMU = 1084  # HES-001086
-PRILOHA_NZ_ZAA = 1074  # HES-001076
-DATA_GEOFYZIKALNI = 1082  # HES-001084
-DATA_GEODETICKA = 1081  # HES-001083
+DATA_GPS = Heslar.objects.get(ident_cely="HES-001085").pk
+EXPERTNI_POSUDEK = Heslar.objects.get(ident_cely="HES-001077").pk
+HLASENI_ZAA = Heslar.objects.get(ident_cely="HES-001080").pk
+TYP_DOKUMENTU_NALEZOVA_ZPRAVA: Final = Heslar.objects.get(ident_cely="HES-001075").pk
+DATA_ANALYZY_EKOFAKTORU = Heslar.objects.get(ident_cely="HES-001082").pk
+INVESTORSKA_ZPRAVA = Heslar.objects.get(ident_cely="HES-001079").pk
+RESTAURATORSKA_ZPRAVA = Heslar.objects.get(ident_cely="HES-001078").pk
+DATA_VEKTOROVEHO_PLANU = Heslar.objects.get(ident_cely="HES-001087").pk
+TEXT_JINY = Heslar.objects.get(ident_cely="HES-001081").pk
+DATA_TERENNIHO_VYZKUMU = Heslar.objects.get(ident_cely="HES-001086").pk
+PRILOHA_NZ_ZAA = Heslar.objects.get(ident_cely="HES-001076").pk
+DATA_GEOFYZIKALNI = Heslar.objects.get(ident_cely="HES-001084").pk
+DATA_GEODETICKA = Heslar.objects.get(ident_cely="HES-001083").pk
 ALLOWED_DOKUMENT_TYPES = [
     DATA_GPS,
     EXPERTNI_POSUDEK,
@@ -54,9 +54,9 @@ ALLOWED_DOKUMENT_TYPES = [
     DATA_GEOFYZIKALNI,
     DATA_GEODETICKA,
 ]
-REKONSTRUKCE_3D_ID: Final = 1111  # HES-001113
-TEXTURA_3D_ID: Final = 1113  # HES-001115
-DOKUMENTACE_3D_ID: Final = 1112  # HES-001114
+REKONSTRUKCE_3D_ID: Final = Heslar.objects.get(ident_cely="HES-001113").pk
+TEXTURA_3D_ID: Final = Heslar.objects.get(ident_cely="HES-001115").pk
+DOKUMENTACE_3D_ID: Final = Heslar.objects.get(ident_cely="HES-001114").pk
 MODEL_3D_DOKUMENT_TYPES = [REKONSTRUKCE_3D_ID, TEXTURA_3D_ID, DOKUMENTACE_3D_ID]
 
 # Nazvy heslaru
@@ -110,12 +110,12 @@ HESLAR_JISTOTA_URCENI: Final = 48
 HESLAR_STAV_DOCHOVANI: Final = 49
 
 # Externi zdroj typ
-EXTERNI_ZDROJ_TYP_KNIHA = 1115  # HES-001117
-EXTERNI_ZDROJ_TYP_CAST_KNIHY = 1116  # HES-001118
-EXTERNI_ZDROJ_TYP_CLANEK_V_CASOPISE = 1117  # HES-001119
-EXTERNI_ZDROJ_TYP_CLANEK_V_NOVINACH = 1118  # HES-001120
-EXTERNI_ZDROJ_TYP_NEPUBLIKOVANA_ZPRAVA = 1119  # HES-001121
+EXTERNI_ZDROJ_TYP_KNIHA = Heslar.objects.get(ident_cely="HES-001117").pk
+EXTERNI_ZDROJ_TYP_CAST_KNIHY = Heslar.objects.get(ident_cely="HES-001118").pk
+EXTERNI_ZDROJ_TYP_CLANEK_V_CASOPISE = Heslar.objects.get(ident_cely="HES-001119").pk
+EXTERNI_ZDROJ_TYP_CLANEK_V_NOVINACH = Heslar.objects.get(ident_cely="HES-001120").pk
+EXTERNI_ZDROJ_TYP_NEPUBLIKOVANA_ZPRAVA = Heslar.objects.get(ident_cely="HES-001121").pk
 
-PIAN_PRESNOST_KATASTR = 862  # HES-000864
-HESLAR_DATUM_SPECIFIKACE_V_LETECH_PRIBLIZNE = 887  # HES-000889
-HESLAR_DATUM_SPECIFIKACE_V_LETECH_PRESNE = 884  # HES-000886
+PIAN_PRESNOST_KATASTR = Heslar.objects.get(ident_cely="HES-000864").pk
+HESLAR_DATUM_SPECIFIKACE_V_LETECH_PRIBLIZNE = Heslar.objects.get(ident_cely="HES-000889").pk
+HESLAR_DATUM_SPECIFIKACE_V_LETECH_PRESNE = Heslar.objects.get(ident_cely="HES-000886").pk
