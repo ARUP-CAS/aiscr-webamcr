dbf256e2b60aaedf6a8f0ecf4601fbd156f756e5
Jan Ihnat <jnihnat@gmail.com>
2023-07-14 21:38:34 +0200

    uprava ez prekladov

--- webclient/ez/models.py
+++ webclient/ez/models.py
@@ -31,9 +31,9 @@ class ExterniZdroj(ExportModelOperationsMixin("externi_zdroj"), ModelWithMetadat
     Class pro db model externí zdroj.
     """
     STATES = (
-        (EZ_STAV_ZAPSANY, _("EZ1 - Zapsána")),
-        (EZ_STAV_ODESLANY, _("EZ2 - Odeslána")),
-        (EZ_STAV_POTVRZENY, _("EZ3 - Potvrzená")),
+        (EZ_STAV_ZAPSANY, _("ez.models.externiZdroj.states.zapsany.label")),
+        (EZ_STAV_ODESLANY, _("ez.models.externiZdroj.states.odeslany.label")),
+        (EZ_STAV_POTVRZENY, _("ez.models.externiZdroj.states.potvrzeny.label")),
     )
 
     sysno = models.TextField(blank=True, null=True)
