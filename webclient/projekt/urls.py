b19a8f84b6bdfeff81639c4edc929f947d83b46a
jiri.pesik <jiri.pesik@outlook.cz>
2023-10-24 10:10:30 +0200

    Hotfix #299 #300 #301 #995

--- webclient/projekt/urls.py
+++ webclient/projekt/urls.py
@@ -59,7 +59,6 @@ urlpatterns = [
         name="projekt_navrhnout_ke_zruseni",
     ),
     path("stav/zrusit/<str:ident_cely>", zrusit, name="projekt_zrusit"),
-    path("stav/vratit/<str:ident_cely>", vratit, name="projekt_vratit"),
     path(
         "stav/vratit-navrh-zruseni/<str:ident_cely>",
         vratit_navrh_zruseni,
@@ -86,7 +85,7 @@ urlpatterns = [
         name="post_ajax_get_project_pian_limit",
     ),
     path(
-        "dokument/odpojit/<str:ident_cely>/<str:ident_cely>",
+        "dokument/odpojit/<str:ident_cely>/<str:proj_ident_cely>",
         odpojit_dokument,
         name="odpojit_dokument",
     ),
