83ef57d7b10164fe70e01f28a33e18e25331d806
Jiří Pešík <jiri.pesik@outlook.cz>
2023-11-17 11:20:29 +0100

    #1069

--- webclient/pas/migrations/0001_initial.py
+++ webclient/pas/migrations/0001_initial.py
@@ -23,12 +23,12 @@ class Migration(migrations.Migration):
                 ('hloubka', models.PositiveIntegerField(blank=True, null=True)),
                 ('geom', django.contrib.gis.db.models.fields.PointField(blank=True, null=True, srid=4326)),
                 ('geom_sjtsk', django.contrib.gis.db.models.fields.PointField(blank=True, null=True, srid=5514)),
-                ('geom_system', models.TextField(default='wgs84')),
+                ('geom_system', models.TextField(default='4326')),
                 ('presna_datace', models.TextField(blank=True, null=True)),
                 ('poznamka', models.TextField(blank=True, null=True)),
                 ('datum_nalezu', models.DateField(blank=True, null=True)),
-                ('stav', models.SmallIntegerField(choices=[(1, 'SN1 - zapsaný'), (2, 'SN2 - odeslaný'), (3, 'SN3 - potvrzený'), (4, 'SN4 - archivovaný')])),
-                ('predano', models.BooleanField(blank=True, choices=[(True, 'Ano'), (False, 'Ne')], default=False, null=True)),
+                ('stav', models.SmallIntegerField(choices=[(1, "pas.models.samostatnyNalez.states.zapsany.label"), (2, "pas.models.samostatnyNalez.states.odeslany.label"), (3, "pas.models.samostatnyNalez.states.potvrzeny.label"), (4, "pas.models.samostatnyNalez.states.archivovany.label")])),
+                ('predano', models.BooleanField(blank=True, choices=[(True, "pas.models.samostatnyNalez.predano.ano"), (False, "pas.models.samostatnyNalez.predano.ne")], default=False, null=True)),
                 ('ident_cely', models.TextField(unique=True)),
                 ('pocet', models.TextField(blank=True, null=True)),
                 ('geom_updated_at', models.DateTimeField(blank=True, null=True)),
@@ -42,7 +42,7 @@ class Migration(migrations.Migration):
             name='UzivatelSpoluprace',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('stav', models.SmallIntegerField(choices=[(1, 'neaktivní'), (2, 'aktivní')])),
+                ('stav', models.SmallIntegerField(choices=[(1, "pas.models.uzivatelSpoluprace.states.neaktivni.label"), (2, "pas.models.uzivatelSpoluprace.states.aktivni.label")])),
                 ('historie', models.OneToOneField(blank=True, db_column='historie', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='spoluprace_historie', to='historie.historievazby')),
             ],
             options={
@@ -50,11 +50,15 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.AddConstraint(
-            model_name='samostatnynalez',
+            model_name="samostatnynalez",
             constraint=models.CheckConstraint(
-                check=models.Q(models.Q(('geom_system', 'sjtsk'), ('geom_sjtsk__isnull', False)),
-                               models.Q(('geom_system', 'wgs84'), ('geom__isnull', False)),
-                               models.Q(('geom_sjtsk__isnull', True), ('geom__isnull', True)), _connector='OR'),
-                name='samostatny_nalez_geom_check'),
+                check=models.Q(
+                    models.Q(("geom_system", "5514"), ("geom_sjtsk__isnull", False)),
+                    models.Q(("geom_system", "4326"), ("geom__isnull", False)),
+                    models.Q(("geom_sjtsk__isnull", True), ("geom__isnull", True)),
+                    _connector="OR",
+                ),
+                name="samostatny_nalez_geom_check",
+            ),
         ),
     ]
