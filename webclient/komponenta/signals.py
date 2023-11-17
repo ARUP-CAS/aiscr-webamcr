0203917d549362c74affd2ed3b39edab0f86b509
Jiří Pešík <jiri.pesik@outlook.cz>
2023-11-17 19:36:22 +0100

    #1310

--- webclient/komponenta/signals.py
+++ webclient/komponenta/signals.py
@@ -9,14 +9,14 @@ from komponenta.models import KomponentaVazby, Komponenta
 logger = logging.getLogger(__name__)
 
 
-@receiver(pre_delete, sender=Komponenta)
-def delete_komponenta_vazby(sender, instance, **kwargs):
+@receiver(pre_delete, sender=KomponentaVazby)
+def delete_komponenta_vazby(sender, instance: KomponentaVazby, **kwargs):
     """
     Náhrada triggeru delete_connected_komponenta_vazby_relations.
     """
     logger.debug("komponenta.signals.delete_komponenta_vazby.start")
     try:
-        KomponentaVazby.objects.filter(komponenty=instance).delete()
+        Komponenta.objects.filter(komponenta_vazby=instance).delete()
     except RecursionError:
         # https://www.appsloveworld.com/django/100/13/django-cascade-delete-on-reverse-foreign-keys
         pass
