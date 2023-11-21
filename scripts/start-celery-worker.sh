2332c7fc125408862e33da3f87e11f2ed794a7d5
jiri.pesik <jiri.pesik@outlook.cz>
2023-11-21 22:43:32 +0100

    docker fix

--- scripts/start-celery-worker.sh
+++ scripts/start-celery-worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # start-celery-worker.sh
 
-crontab -u root /scripts/crontab-worker.txt
+sudo crontab -u root /scripts/crontab-worker.txt
 celery -A webclient worker -l INFO -n worker1@amcr
