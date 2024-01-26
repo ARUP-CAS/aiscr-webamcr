c648dda0d174fc8dfb82ef4a6215f284e9cc8feb
jnihnat <57906106+jnihnat@users.noreply.github.com>
2024-01-26 15:40:47 +0100

    opravy (#1489)
    Co-authored-by: Jiří Pešík <jiri.pesik@outlook.cz>

--- webclient/webclient/celery.py
+++ webclient/webclient/celery.py
@@ -4,6 +4,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab   
 
 
 if os.getenv("DJANGO_SETTINGS_MODULE") is None:
@@ -12,5 +13,12 @@ if os.getenv("DJANGO_SETTINGS_MODULE") is None:
 app = Celery("webclient")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.beat_schedule = {
+    'send-notifications-E-N-01-02': {
+        'task': 'cron.tasks.send_notifications',
+        'schedule': crontab(hour=2)
+    },
+}
 
 app.autodiscover_tasks()
