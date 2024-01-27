5459c0ab830a17ec981c209bc7ea925b7dcca8f4
Jiří Pešík <jiri.pesik@outlook.cz>
2024-01-27 13:07:47 +0100

    Update schema and Soubor model #1167 (#1496)
    * Update schema and Soubor model #1167
    
    * Update schema and Soubor model #1167

--- webclient/historie/signals.py
+++ webclient/historie/signals.py
@@ -14,7 +14,9 @@ def soubor_update_metadata(sender, instance: Historie, **kwargs):
     """
     Funkce pro uložení metadat k objektům navázaným na soubor
     """
-    if not instance.suppress_signal:
+    if (not hasattr(instance, "suppress_signal") or instance.suppress_signal is False) and (
+            hasattr(instance.vazba.navazany_objekt, "suppress_signall")
+            or instance.vazba.navazany_objekt.suppress_signal is False):
         navazany_objekt = instance.vazba.navazany_objekt
         if isinstance(navazany_objekt, ModelWithMetadata):
             navazany_objekt.save_metadata()
