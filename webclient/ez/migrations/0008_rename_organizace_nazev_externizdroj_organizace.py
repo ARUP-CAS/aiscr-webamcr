56d6b4273e289187befcae272c223b352269e343
pesikj <jiri.pesik@outlook.cz>
2024-01-30 22:48:07 +0100

    Models update, migration, XSD update #1467

--- webclient/ez/migrations/0008_rename_organizace_nazev_externizdroj_organizace.py
+++ webclient/ez/migrations/0008_rename_organizace_nazev_externizdroj_organizace.py
@@ -3,12 +3,6 @@
 from django.db import migrations
 
 
-def populate_organizace_nazev(apps, schema_editor):
-    ExterniZdroj = apps.get_model('ez', 'ExterniZdroj')
-    for instance in ExterniZdroj.objects.all(organizace__isnull=False):
-        instance.save()
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ("ez", "0007_remove_externizdroj_organizace_nazev_and_more"),
