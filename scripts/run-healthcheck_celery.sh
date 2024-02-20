8b2df4a111a75fc18be8e9ee1740a5f5de2a7a6b
Jiří Pešík <jiri.pesik@outlook.cz>
2024-02-20 18:04:39 +0100

    Docker update

--- scripts/run-healthcheck_celery.sh
+++ scripts/run-healthcheck_celery.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+CELERY_APP_PATH="webclient"
 
-celery -A webclient inspect ping &> /dev/null
-result="$?"
+dates=$(celery -A $CELERY_APP_PATH inspect scheduled | grep 'eta' | awk '{print $3}' | tr -d "',")
+current_seconds=$(date +%s)
+greatest_diff=0
+while IFS= read -r line; do
+    # Skip empty lines
+    if [ -z "$line" ]; then continue; fi
+    date_seconds=$(date -d"$line" +%s)
+    seconds_diff=$((current_seconds - date_seconds))
+    if [ $seconds_diff -gt $greatest_diff ]; then
+        greatest_diff=$seconds_diff
+    fi
+done <<< "$dates"
 
+dates=$(celery -A $CELERY_APP_PATH inspect scheduled | grep 'eta' | awk '{print $3}' | tr -d "',")
+while IFS= read -r line; do
+    # Skip empty lines
+    if [ -z "$line" ]; then continue; fi
+    date_seconds=$(date -d"$line" +%s)
+    seconds_diff=$((current_seconds - date_seconds))
+    if [ $seconds_diff -gt $greatest_diff ]; then
+        greatest_diff=$seconds_diff
+    fi
+done <<< "$dates"
 
-if [ ${result} -eq 0 ]; then
-  echo "Celery PING OK"
-  exit 0
+if [ "$greatest_diff" -lt 120 ]; then
+    exit 0
 else
-  echo "Celery PING FAIL"
-  exit 1
+    exit 1
 fi
