8d0d1b164b426409f4e2cbb1d81e92919b6a03ae
Jiří Pešík <jiri.pesik@outlook.cz>
2024-02-21 15:41:28 +0100

    Fedora healthcheck #1523 (#1605)
    * Fedora healthcheck #1523
    
    * Fedora healthcheck #1523

--- scripts/run-healthcheck.sh
+++ scripts/run-healthcheck.sh
@@ -28,7 +28,23 @@ status_code="$(curl -s -L -o /dev/null -w '%{http_code}'  127.0.0.1:8001/healthc
 
 echo "HTTP status code from /healthcheck endpoint is ${status_code}"
 
-test "${status_code}" -eq "200" && python /scripts/db/db_connection_from_docker-web.py && test ${disk_status} -eq 0 && test ${ram_status} -eq 0 && true || false
+if [ -f "/run/secrets/db_conf" ]; then
+    file_path="/run/secrets/db_conf"
+else
+    file_path="/code/webclient/settings/sample_secrets_db.json"
+fi
+fedora_server_hostname=$(jq -r '.FEDORA_SERVER_HOSTNAME' "$file_path")
+fedora_server_name=$(jq -r '.FEDORA_SERVER_NAME' "$file_path")
+fedora_port_number=$(jq -r '.FEDORA_PORT_NUMBER' "$file_path")
+fedora_user=$(jq -r '.FEDORA_ADMIN_USER' "$file_path")
+fedora_user_password=$(jq -r '.FEDORA_ADMIN_USER_PASSWORD' "$file_path")
+
+url="http://${fedora_server_hostname}:${fedora_port_number}/rest/${fedora_server_name}"
+fedora_status_code=$(curl -o /dev/null -s -w "%{http_code}" -u "${fedora_user}":"${fedora_user_password}" "$url")
+
+echo "HTTP status code from FEDORA is ${fedora_status_code}"
+
+test "${status_code}" -eq "200" && "${fedora_status_code}" -eq "200" && python /scripts/db/db_connection_from_docker-web.py && test ${disk_status} -eq 0 && test ${ram_status} -eq 0 && true || false
 
 exit_code="$?"
 
