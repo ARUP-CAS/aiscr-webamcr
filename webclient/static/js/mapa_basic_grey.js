b868c5211d0b0a2a0f2bb83e578cfe550e700032
Jan Havrlant <155644357+jhavrlant@users.noreply.github.com>
2024-03-08 13:53:50 +0100

    Odstranění lomítka, které už je už v static_url (#1638)

--- webclient/static/js/mapa_basic_grey.js
+++ webclient/static/js/mapa_basic_grey.js
@@ -114,7 +114,7 @@ var searchControl=new L.Control.Search({
 let global_measuring_toolbox=new L.control.measure(
     {
         title: [map_translations['MeasureTitle']],
-        icon:'<img src="'+static_url+'/img/ruler-bold-32.png" style="width:20px"/>'
+        icon:'<img src="'+static_url+'img/ruler-bold-32.png" style="width:20px"/>'
     });
 map.addControl(global_measuring_toolbox);
 
