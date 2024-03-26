4124235d4d1fcc603910f1401dd37c8dc4e8b22e
Jiří Pešík <jiri.pesik@outlook.cz>
2024-03-26 14:28:38 +0100

    #1043

--- webclient/webclient/settings/dev.py
+++ webclient/webclient/settings/dev.py
@@ -31,7 +31,7 @@ DEBUG_TOOLBAR_PANELS = [
 
 # This is only so that debug toolbar is shown when developing in docker
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2", "localhost", '192.168.1.1']
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
 CRISPY_FAIL_SILENTLY = not DEBUG
 
