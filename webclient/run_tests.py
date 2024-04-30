1a965c4741fc60b52f97c551cc23e7601c05a612
Jan Havrlant <jhavrlant@arup.cas.cz>
2024-04-30 08:42:11 +0200

    Přidaná možnost spustit neúspěšné testy

--- webclient/run_tests.py
+++ webclient/run_tests.py
@@ -1,8 +1,15 @@
 import logging
 import os
 import subprocess
-
+from threading import Thread
 import logstash
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f','--fail', help='provede neúspěšné testy', action='store_true')
+parser.add_argument('-a','--all', help='provede všechny testy', action='store_true')
+
+args = parser.parse_args()
 
 SETTINGS = "webclient.settings.dev_test"
 
@@ -11,29 +18,51 @@ if os.path.exists(test_preparation_path):
     subprocess.run(f"./{test_preparation_path}")
 
 logger = logging.getLogger('webamcr.test_runner')
-logger.setLevel(logging.INFO)
+#logger.setLevel(logging.INFO)
 logger.addHandler(logstash.TCPLogstashHandler(host='logstash', version=1, tags="dev_test"))
 logger.info('amcr_test_runner.start')
 
-test_list = [
+if args.fail==True:
+    from webclient.settings.dev_test import TEST_SCREENSHOT_PATH
+    import pandas
+    if os.path.isfile(f'{TEST_SCREENSHOT_PATH}results.xlsx'):
+            data= pandas.read_excel(f'{TEST_SCREENSHOT_PATH}results.xlsx')
+            d= data.values.tolist()
+            test_list = []
+            for line in d:
+                if(line[3]!="OK" and isinstance(line[3], float)!=True ):
+                    test_list.append(line[2])
+            if len(test_list)==0:
+                print("Všechny testy OK")
+                exit()
+                
+    else:
+        print("Nenalezn soubor s výsledky")
+        exit()
+    
+else:    
+    test_list = [
     "core.tests.test_selenium",
     "projekt.tests.test_selenium",
     "dokument.tests.test_selenium",
     "lokalita.tests.test_selenium",  
     "arch_z.tests.test_selenium",  
-    "pas.tests.test_selenium",      
-]
+    "pas.tests.test_selenium",       
+    ]
 
-test_count = 0
-for test_file  in test_list:
+
+def reader(pipe):
+    while True:
+        line = pipe.read(1)
+        print(line,end="")
+        if not line:
+            break
     
-    logger.info("amcr_test_runner.start_test", extra={"test": f"{test_file}"})
-    process = subprocess.Popen(f"python3 manage.py test {test_file} --settings={SETTINGS} --noinput", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for line in process.stdout:
-        print(line, end="")
-    for line in process.stderr:
-        print(line, end="")
-    process.wait()    
-    logger.info("amcr_test_runner.end_test", extra={"test_file": test_file} )
-    test_count += 1
-logger.info("amcr_test_runner.end", extra={"test_count": test_count})
+logger.info("amcr_test_runner.start_test", extra={"test": f"{' '.join(test_list)}"})
+process = subprocess.Popen(f"python3 manage.py test {' '.join(test_list)} --settings={SETTINGS} --noinput --verbosity=2", stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True )
+
+t1=Thread(target=reader, args=[process.stdout]).start()
+t2=Thread(target=reader, args=[process.stderr]).start()
+process.wait()
+
+logger.info("amcr_test_runner.end")
