6b70dff49298875f080a5c7c74cde2fb14803bfd
jnihnat <57906106+jnihnat@users.noreply.github.com>
2024-05-13 18:10:16 +0200

    oprava (#1821)

--- webclient/core/views.py
+++ webclient/core/views.py
@@ -795,7 +795,6 @@ class PermissionFilterMixin():
                 qs = new_qs | qs.filter(**filterdoc)
             else:
                 qs = new_qs
-        qs = qs.cache()
         return qs
     
     def filter_by_permission(self, qs, permission):
