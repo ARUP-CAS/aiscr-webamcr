e52ee12495dc976d31fb505b51efacf38904ebac
Jan Ihnat <jnihnat@gmail.com>
2024-05-13 10:42:56 +0200

    oprava

--- webclient/pas/views.py
+++ webclient/pas/views.py
@@ -910,7 +910,7 @@ class AktivaceEmailView(LoginRequiredMixin, DetailView):
         obj: UzivatelSpoluprace = self.get_object()
         if not obj.aktivni:
             obj.set_aktivni(request.user)
-        return redirect(reverse("pas:spoluprace_aktivace", kwargs={"pk": obj.pk}))
+        return redirect(reverse("pas:spoluprace_list")+"?sort=organizace_vedouci&sort=spolupracovnik")
 
 
 @login_required
