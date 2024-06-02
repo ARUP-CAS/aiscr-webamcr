2bf50121440404511b28feb410e9d955a1642608
Jiří Pešík <jiri.pesik@outlook.cz>
2024-06-02 14:56:41 +0200

    #1566 (#1902)

--- webclient/static/js/pridani_title_pro_disabled.js
+++ webclient/static/js/pridani_title_pro_disabled.js
@@ -6,8 +6,8 @@ function initializeTooltips() {
 }
 
 window.onload = function () {
-    inputs = document.querySelectorAll("input[type=text]");
-    for (index = 0; index < inputs.length; ++index) {
+    const inputs = document.querySelectorAll("input[type=text]");
+    for (let index = 0; index < inputs.length; ++index) {
         if (inputs[index].readOnly === true) {
             inputs[index].title = inputs[index].value
             inputs[index].setAttribute("data-toggle", "tooltip")
@@ -15,13 +15,23 @@ window.onload = function () {
             inputs[index].setAttribute("rel", "tooltip")
         }
     }
-    textareas = document.querySelectorAll("textarea");
-    for (index = 0; index < textareas.length; ++index) {
+    const textareas = document.querySelectorAll("textarea");
+    for (let index = 0; index < textareas.length; ++index) {
         if (inputs[index].readOnly === true) {
             textareas[index].title = textareas[index].value
             textareas[index].setAttribute("data-toggle", "tooltip")
             textareas[index].setAttribute("data-placement", "top")
             inputs[index].setAttribute("rel", "tooltip")
+        }
+    }
+    const elements = document.querySelectorAll("[name*='visible_x']");
+    for (let index = 0; index < elements.length; ++index) {
+        const element = elements[index]
+        if (element.disabled) {
+            element.title = element.value
+            element.setAttribute("data-toggle", "tooltip")
+            element.setAttribute("data-placement", "top")
+            element.setAttribute("rel", "tooltip")
         }
     }
     initializeTooltips()
