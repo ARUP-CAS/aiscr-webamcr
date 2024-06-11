7c76c59b5a99e132f7c0297a0e175cc772450728
Jan Havrlant <jhavrlant@arup.cas.cz>
2024-06-11 17:48:54 +0200

    Upravené funkce pro migraci

--- webclient/cron/tasks.py
+++ webclient/cron/tasks.py
@@ -15,8 +15,7 @@ from core.connectors import RedisConnector
 from core.constants import ODESLANI_SN, ARCHIVACE_SN, PROJEKT_STAV_ZRUSENY, RUSENI_PROJ, PROJEKT_STAV_VYTVORENY, \
     OZNAMENI_PROJ, ZAPSANI_PROJ, ARCHEOLOGICKY_ZAZNAM_RELATION_TYPE, RUSENI_STARE_PROJ, UDAJ_ODSTRANEN, STARY_PROJEKT_ZRUSEN
 from core.models import Soubor
-from core.coordTransform import get_multi_transform_to_sjtsk , get_multi_transform_to_wgs84
-from cron.classes import MyList
+from core.coordTransform import transform_geom_to_sjtsk
 from cron.functions import collect_en01_en02
 from django.db import connection, transaction
 from django.utils.translation import gettext as _
@@ -37,10 +36,6 @@ from uzivatel.models import Organizace, Osoba, User
 from webclient.settings.base import get_plain_redis_pass
 
 logger = logging.getLogger(__name__)
-
-
-NUM_TO_WGS84_CONVERT = 200
-NUM_TO_SJTSK_CONVERT = 200
 
 
 @shared_task
@@ -73,332 +68,65 @@ def send_notifications():
 
 @shared_task
 def pian_to_sjtsk():
-    try:
-        count_selected_wgs84 = 0
-        count_updated_sjtsk = 0
-        count_error_sjtsk = 0
-        query_select = (
-            "select pian.id,pian.ident_cely,ST_AsText(pian.geom) as geometry,ST_AsText(pian.geom_sjtsk) as geometry_sjtsk "
-            " from public.pian pian "
-            " where pian.geom is not null "
-            " and (pian.geom_sjtsk is null or geom_system in ('5514*','sjtsk*'))"
-            " and pian.id not in (select pian_id from public.amcr_geom_migrations_jobs_wgs84_errors)"
-            " order by pian.id"
-            " limit %s"
-        )
-        query_update = (
-            "update public.pian pian "
-            " set geom_sjtsk = ST_GeomFromText(%s), geom_sjtsk_updated_at=CURRENT_TIMESTAMP "
-            " where pian.geom_sjtsk is null and pian.id=%s "
-            " and ST_AsText(pian.geom)=%s"
-        )
-        query_insert_err = "insert into public.amcr_geom_migrations_jobs_wgs84_errors(pian_id) values(%s)"
-        query_insert_stat = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_wgs84,count_updated_sjtsk,count_error_sjtsk) values(%s,%s,%s,%s)"
-        query_delete_jobs = "delete from public.amcr_geom_migrations_jobs where typ='pian:wgs84->sjtsk' and created_at<CURRENT_TIMESTAMP - INTERVAL '1' hour and count_selected_wgs84=0 and count_selected_sjtsk=0"
 
-        try:
-            l = MyList()
-            xx = []
-            pians = Pian.objects.raw(query_select, [NUM_TO_SJTSK_CONVERT])
-            for pian in pians:
-                count_selected_wgs84 += 1
-                l.add(pian.id, pian.geometry)
-                xx.append(pian.geometry)
-            if count_selected_wgs84 > 0:
-                l2 = l.simple_list()
-                l3 = get_multi_transform_to_sjtsk(l2)
-                l4 = l.geom_list(l3)
-                for i in range(0, len(l4)):
-                    logger.debug("+" + l4[i][1] + "  " + str(l4[i][0]))
-                    if l4[i][1] == "ERROR":
-                        with connection.cursor() as cursor:
-                            count_error_sjtsk += 1
-                            cursor.execute(query_insert_err, [l4[i][0]])
-                    else:
-                        with connection.cursor() as cursor:
-                            count_updated_sjtsk += 1
-                            cursor.execute(
-                                query_update, [l4[i][1], l4[i][0], xx[i]]
-                            )
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        query_insert_stat,
-                        [
-                            "pian:wgs84->sjtsk",
-                            count_selected_wgs84,
-                            count_updated_sjtsk,
-                            count_error_sjtsk,
-                        ],
-                    )
-                    cursor.execute(query_delete_jobs)
-        except Exception as e:
-            logger.debug("Error during migrations pians wgs84->sJTSK")
-            logger.debug(e)
-            logger.debug(traceback.format_exc())
-            return None
-    except Exception as err:
-        logger.error("cron.pian_to_sjtsk.do.error", extra={"error": err})
+    query_select = (
+        "select pian.id,pian.ident_cely,ST_AsText(pian.geom) as geometry,ST_AsText(pian.geom_sjtsk) as geometry_sjtsk "
+        " from public.pian pian "
+        " where pian.geom is not null "
+        " and (pian.geom_sjtsk is null or geom_system in ('5514*','sjtsk*'))"
+        " and pian.id not in (select pian_id from public.amcr_geom_migrations_jobs_wgs84_errors)"
+        " order by pian.id"       
+    )
+    query_update = (
+        "update public.pian pian "
+        " set geom_sjtsk = ST_GeomFromText(%s), geom_sjtsk_updated_at=CURRENT_TIMESTAMP "
+        " where pian.geom_sjtsk is null and pian.id=%s "        
+    )
+    pians = Pian.objects.raw(query_select)
+    c=len(pians)
+    for idx, pian in enumerate(pians):
+        if idx%(c//100)==0:
+            print(f'\r{round(idx/c*100)}%', end='')
+        geom = transform_geom_to_sjtsk(pian.geometry)
+        if geom[1]=='OK':
+            with connection.cursor() as cursor:
+                cursor.execute(
+                                query_update, [geom[0], pian.id]
+                        )
+        else:
+            print("chyba pian id {pian.id}")
+            
 
 
 @shared_task
-def pian_to_wsg_84():
-    try:
-        count_selected_sjtsk = 0
-        count_selected_wgs84 = 0
-        count_error_wgs84 = 0
-        query_select = (
-            "select pian.id,pian.ident_cely,ST_AsText(pian.geom) as geometry,ST_AsText(pian.geom_sjtsk) as geometry_sjtsk "
-            " from public.pian pian "
-            " where pian.geom is null "
-            " and pian.geom_sjtsk is not null "
-            " and pian.id not in (select pian_id from public.amcr_geom_migrations_jobs_sjtsk_errors)"
-            " order by pian.id"
-            " limit %s"
-        )
-        query_update = (
-            "update public.pian pian "
-            " set geom = ST_GeomFromText(%s), geom_updated_at=CURRENT_TIMESTAMP "
-            " where pian.geom is null and pian.id=%s "
-            " and ST_AsText(pian.geom_sjtsk)=%s"
-        )
-        query_insert_err = "insert into public.amcr_geom_migrations_jobs_sjtsk_errors(pian_id) values(%s)"
-        query_insert_stat = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_sjtsk,count_selected_wgs84,count_error_wgs84) values(%s,%s,%s,%s)"
-        query_insert_stat2 = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_sjtsk,detail) values(%s,%s,%s)"
-        query_delete_jobs = "delete from public.amcr_geom_migrations_jobs where typ='pian:sjtsk->wgs84' and created_at<CURRENT_TIMESTAMP - INTERVAL '1' hour and count_selected_sjtsk=0 and count_selected_sjtsk=0"
-
-        try:
-            l = MyList()
-            xx = []
-            pians = Pian.objects.raw(query_select, [NUM_TO_WGS84_CONVERT])
-            for pian in pians:
-                count_selected_sjtsk += 1
-                l.add(pian.id, pian.geometry_sjtsk)
-                xx.append(pian.geometry_sjtsk)
-            if count_selected_sjtsk > 0:
-                l2 = l.simple_list()
-                l3 = get_multi_transform_to_wgs84(l2)
-                if l3 is not None:
-                    l4 = l.geom_list(l3)
-                    for i in range(0, len(l4)):
-                        logger.debug("+" + l4[i][1] + "  " + str(l4[i][0]))
-                        if l4[i][1] == "ERROR":
-                            with connection.cursor() as cursor:
-                                count_error_wgs84 += 1
-                                cursor.execute(query_insert_err, [l4[i][0]])
-                        else:
-                            with connection.cursor() as cursor:
-                                count_selected_wgs84 += 1
-                                cursor.execute(
-                                    query_update, [l4[i][1], l4[i][0], xx[i]]
-                                )
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            query_insert_stat,
-                            [
-                                "pian:sjtsk->wgs84",
-                                count_selected_sjtsk,
-                                count_selected_wgs84,
-                                count_error_wgs84,
-                            ],
-                        )
-                        cursor.execute(query_delete_jobs)
-                else:
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            query_insert_stat2,
-                            [
-                                "pian:sjtsk->wgs84",
-                                count_selected_sjtsk,
-                                "Error during connection to CUZK",
-                            ],
-                        )
-                        cursor.execute(query_delete_jobs)
-            else:
-                with connection.cursor() as cursor:
+def nalez_to_sjtsk():    
+    query_select = (
+        "select samostatny_nalez.id,samostatny_nalez.ident_cely,ST_AsText(samostatny_nalez.geom) as geometry,ST_AsText(samostatny_nalez.geom_sjtsk) as geometry_sjtsk "
+        " from public.samostatny_nalez "
+        " where samostatny_nalez.geom is not null "
+        " and (samostatny_nalez.geom_sjtsk is null or geom_system in ('5514*','sjtsk*'))"
+        " and samostatny_nalez.id not in (select pian_id from public.amcr_geom_migrations_jobs_wgs84_errors)"
+        " order by samostatny_nalez.id"        
+    )
+    query_update = (
+        "update public.samostatny_nalez "
+        " set geom_sjtsk = ST_GeomFromText(%s), geom_sjtsk_updated_at=CURRENT_TIMESTAMP "
+        " where samostatny_nalez.geom_sjtsk is null and samostatny_nalez.id=%s "        
+    )
+    SNs = SamostatnyNalez.objects.raw( query_select )
+    c=len(SNs)
+    for idx, SN in enumerate(SNs): 
+        if idx%(c//100)==0:
+            print(f'\r{round(idx/c*100)}%', end='') 
+            
+        geom = transform_geom_to_sjtsk(SN.geometry)           
+        if geom[1]=='OK':
+            with connection.cursor() as cursor:
                     cursor.execute(
-                        query_insert_stat,
-                        [
-                            "pian:sjtsk->wgs84",
-                            count_selected_sjtsk,
-                            count_selected_wgs84,
-                            count_error_wgs84,
-                        ],
+                        query_update, [geom[0], SN.id]
                     )
-                    cursor.execute(query_delete_jobs)
-        except Exception as e:
-            logger.debug("Error during migrations pians sjtsk->wgs84")
-            logger.debug(e)
-            logger.debug(traceback.format_exc())
-            return None
-    except Exception as err:
-        logger.error("cron.pian_to_wsg_84.do.error", extra={"error": err})
-
-
-@shared_task
-def nalez_to_sjtsk():
-    try:
-        count_selected_wgs84 = 0
-        count_updated_sjtsk = 0
-        count_error_sjtsk = 0
-        query_select = (
-            "select samostatny_nalez.id,samostatny_nalez.ident_cely,ST_AsText(samostatny_nalez.geom) as geometry,ST_AsText(samostatny_nalez.geom_sjtsk) as geometry_sjtsk "
-            " from public.samostatny_nalez "
-            " where samostatny_nalez.geom is not null "
-            " and (samostatny_nalez.geom_sjtsk is null or geom_system in ('5514*','sjtsk*'))"
-            " and samostatny_nalez.id not in (select pian_id from public.amcr_geom_migrations_jobs_wgs84_errors)"
-            " order by samostatny_nalez.id"
-            " limit %s"
-        )
-        query_update = (
-            "update public.samostatny_nalez "
-            " set geom_sjtsk = ST_GeomFromText(%s), geom_sjtsk_updated_at=CURRENT_TIMESTAMP "
-            " where samostatny_nalez.geom_sjtsk is null and samostatny_nalez.id=%s "
-            " and ST_AsText(samostatny_nalez.geom)=%s"
-        )
-        query_insert_err = "insert into public.amcr_geom_migrations_jobs_wgs84_errors(pian_id) values(%s)"
-        query_insert_stat = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_wgs84,count_updated_sjtsk,count_error_sjtsk) values(%s,%s,%s,%s)"
-        query_delete_jobs = "delete from public.amcr_geom_migrations_jobs where typ='samostatny_nalez:wgs84->sjtsk' and created_at<CURRENT_TIMESTAMP - INTERVAL '1' hour and count_selected_wgs84=0 and count_selected_sjtsk=0"
-
-        try:
-            l = MyList()
-            xx = []
-            SNs = SamostatnyNalez.objects.raw(
-                query_select, [NUM_TO_SJTSK_CONVERT]
-            )
-            for SN in SNs:
-                count_selected_wgs84 += 1
-                l.add(SN.id, SN.geometry)
-                xx.append(SN.geometry)
-            if count_selected_wgs84 > 0:
-                l2 = l.simple_list()
-                l3 = get_multi_transform_to_sjtsk(l2)
-                l4 = l.geom_list(l3)
-                for i in range(0, len(l4)):
-                    logger.debug("+" + l4[i][1] + "  " + str(l4[i][0]))
-                    if l4[i][1] == "ERROR":
-                        with connection.cursor() as cursor:
-                            count_error_sjtsk += 1
-                            cursor.execute(query_insert_err, [l4[i][0]])
-                    else:
-                        with connection.cursor() as cursor:
-                            count_updated_sjtsk += 1
-                            cursor.execute(
-                                query_update, [l4[i][1], l4[i][0], xx[i]]
-                            )
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        query_insert_stat,
-                        [
-                            "samostatny_nalez:wgs84->sjtsk",
-                            count_selected_wgs84,
-                            count_updated_sjtsk,
-                            count_error_sjtsk,
-                        ],
-                    )
-                    cursor.execute(query_delete_jobs)
-        except Exception as e:
-            logger.debug("Error during migrations pians wgs84->sJTSK")
-            logger.debug(e)
-            logger.debug(traceback.format_exc())
-            return None
-    except Exception as err:
-        logger.error("cron.nalez_to_sjtsk.do.error", extra={"error": err})
-
-
-@shared_task
-def nalez_to_wsg84():
-    try:
-        count_selected_sjtsk = 0
-        count_selected_wgs84 = 0
-        count_error_wgs84 = 0
-        query_select = (
-            "select samostatny_nalez.id,samostatny_nalez.ident_cely,ST_AsText(samostatny_nalez.geom) as geometry,ST_AsText(samostatny_nalez.geom_sjtsk) as geometry_sjtsk "
-            " from public.samostatny_nalez "
-            " where samostatny_nalez.geom is null "
-            " and samostatny_nalez.geom_sjtsk is not null "
-            " and samostatny_nalez.id not in (select pian_id from public.amcr_geom_migrations_jobs_sjtsk_errors)"
-            " order by samostatny_nalez.id"
-            " limit %s"
-        )
-        query_update = (
-            "update public.samostatny_nalez "
-            " set geom = ST_GeomFromText(%s), geom_updated_at=CURRENT_TIMESTAMP "
-            " where samostatny_nalez.geom is null and samostatny_nalez.id=%s "
-            " and ST_AsText(samostatny_nalez.geom_sjtsk)=%s"
-        )
-        query_insert_err = "insert into public.amcr_geom_migrations_jobs_sjtsk_errors(pian_id) values(%s)"
-        query_insert_stat = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_sjtsk,count_selected_wgs84,count_error_wgs84) values(%s,%s,%s,%s)"
-        query_insert_stat2 = "insert into public.amcr_geom_migrations_jobs(typ,count_selected_sjtsk,detail) values(%s,%s,%s)"
-        query_delete_jobs = "delete from public.amcr_geom_migrations_jobs where typ='samostatny_nalez:sjtsk->wgs84' and created_at<CURRENT_TIMESTAMP - INTERVAL '1' hour and count_selected_sjtsk=0 and count_selected_sjtsk=0"
-
-        try:
-            l = MyList()
-            xx = []
-            SNs = SamostatnyNalez.objects.raw(
-                query_select, [NUM_TO_WGS84_CONVERT]
-            )
-            for SN in SNs:
-                count_selected_sjtsk += 1
-                l.add(SN.id, SN.geometry_sjtsk)
-                xx.append(SN.geometry_sjtsk)
-            if count_selected_sjtsk > 0:
-                l2 = l.simple_list()
-                l3 = get_multi_transform_to_wgs84(l2)
-                if l3 is not None:
-                    l4 = l.geom_list(l3)
-                    for i in range(0, len(l4)):
-                        logger.debug("+" + l4[i][1] + "  " + str(l4[i][0]))
-                        if l4[i][1] == "ERROR":
-                            with connection.cursor() as cursor:
-                                count_error_wgs84 += 1
-                                cursor.execute(query_insert_err, [l4[i][0]])
-                        else:
-                            with connection.cursor() as cursor:
-                                count_selected_wgs84 += 1
-                                cursor.execute(
-                                    query_update, [l4[i][1], l4[i][0], xx[i]]
-                                )
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            query_insert_stat,
-                            [
-                                "samostatny_nalez:sjtsk->wgs84",
-                                count_selected_sjtsk,
-                                count_selected_wgs84,
-                                count_error_wgs84,
-                            ],
-                        )
-                        cursor.execute(query_delete_jobs)
-                else:
-                    with connection.cursor() as cursor:
-                        cursor.execute(
-                            query_insert_stat2,
-                            [
-                                "samostatny_nalez:sjtsk->wgs84",
-                                count_selected_sjtsk,
-                                "Error during connection to CUZK",
-                            ],
-                        )
-                        cursor.execute(query_delete_jobs)
-            else:
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        query_insert_stat,
-                        [
-                            "samostatny_nalez:sjtsk->wgs84",
-                            count_selected_sjtsk,
-                            count_selected_wgs84,
-                            count_error_wgs84,
-                        ],
-                    )
-                    cursor.execute(query_delete_jobs)
-        except Exception as e:
-            logger.debug("Error during migrations pians sjtsk->wgs84")
-            logger.debug(e)
-            logger.debug(traceback.format_exc())
-            return None
-    except Exception as err:
-        logger.error("cron.nalez_to_wsg84.do.error", extra={"error": err})
+        else:
+            print("chyba SN id {SN.id}")
 
 
 @shared_task
