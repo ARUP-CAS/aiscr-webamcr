9cf11c0dd2806c640da790aef32d88f93f99fa4b
Jiří Pešík <jiri.pesik@outlook.cz>
2024-06-16 13:39:41 +0200

    Bugfix/1842 (#1951)
    * #1842
    
    * #1842
    
    * #1939

--- webclient/notifikace_projekty/signals.py
+++ webclient/notifikace_projekty/signals.py
@@ -3,7 +3,7 @@ import logging
 from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 
-from core.repository_connector import FedoraTransaction
+from core.repository_connector import FedoraTransaction, FedoraError
 from notifikace_projekty.models import Pes
 
 
@@ -23,7 +23,12 @@ def pes_save(sender, instance: Pes, **kwargs):
 def pes_delete(sender, instance: Pes, **kwargs):
     if instance.user and not getattr(instance, "suppress_signal", False):
         transaction = FedoraTransaction()
-        instance.user.save_metadata(transaction, close_transaction=True)
+        try:
+            instance.user.save_metadata(transaction, close_transaction=True)
+        except FedoraError as err:
+            # Occurs when record is update via admin interface
+            logger.debug("notifikace_projekty.signals.pes_delete.save_metadata.error",
+                         extra={"err": err})
         logger.debug("notifikace_projekty.signals.pes_delete.save_metadata.end",
                      extra={"transaction": getattr(transaction, "uid", None)})
 
