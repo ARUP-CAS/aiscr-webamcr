c464bb221b050262a2d5101d77d052cbd3d3dfbe
Jiří Pešík <jiri.pesik@outlook.cz>
2024-07-05 13:04:09 +0200

    Script fix

--- scripts/run-healthcheck.sh
+++ scripts/run-healthcheck.sh
@@ -14,16 +14,6 @@ else
   ram_status=1
 fi
 
-disk_utilization=$(df -h | awk '$NF=="/"{printf "%s", $5}' | cut -d'%' -f1)
-
-if [ $disk_utilization -lt ${disk_limit} ]; then
-  echo "Disk utilization is lower than ${disk_limit}% ($disk_utilization%)."
-  disk_status=0
-else
-  echo "Disk utilization is higher than or equal to ${disk_limit}% ($disk_utilization%)."
-  disk_status=1
-fi
-
 status_code="$(curl -s -L -o /dev/null -w '%{http_code}'  127.0.0.1:8001/healthcheck)"
 
 echo "HTTP status code from /healthcheck endpoint is ${status_code}"
@@ -51,7 +41,7 @@ exit_code="$?"
 
 
 if [ ${exit_code} -eq 0 ] ; then
-    echo "Django application is running with RAM ${rounded_ram_utilization} % and DISK utilization ${disk_utilization}%"
+    echo "Django application is running with RAM ${rounded_ram_utilization} %."
     exit 0
 else
     echo "Django application is not running or RAM or DISK limits exceeded"
