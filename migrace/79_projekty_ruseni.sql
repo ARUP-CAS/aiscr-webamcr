b4768316bb23c47bbc1f2ab79cde4936ebc9199d
David Novák <novak@arup.cas.cz>
2024-07-07 20:13:01 +0200

    Update 79_projekty_ruseni.sql

--- migrace/79_projekty_ruseni.sql
+++ migrace/79_projekty_ruseni.sql
@@ -1,23 +1,31 @@
 # Zrušení starých projektů
-WITH zrus AS
-(
-    SELECT DISTINCT pr.historie FROM projekt pr
-    JOIN historie h ON pr.historie = h.vazba
-    WHERE pr.stav = 1 AND h.typ_zmeny in ('PX0', 'PX1') AND h.datum_zmeny < (now() - INTERVAL '3 year') AND upper(pr.planovane_zahajeni) < (now() - interval '1 year')
+WITH zrus AS (
+	SELECT DISTINCT pr.historie FROM projekt pr
+		JOIN historie h ON pr.historie = h.vazba
+	WHERE pr.stav = 1 AND pr.typ_projektu = (SELECT id FROM heslar WHERE ident_cely = 'HES-001136') AND h.typ_zmeny in ('PX0', 'PX1') AND h.datum_zmeny < (now() - INTERVAL '3 year') AND upper(pr.planovane_zahajeni) < (now() - interval '1 year')
 )
 INSERT INTO historie (datum_zmeny, uzivatel, poznamka, vazba, typ_zmeny)
 SELECT now(), (SELECT id FROM auth_user WHERE email = 'amcr@arup.cas.cz'), 'Automatické zrušení projektů starších tří let, u kterých již nelze očekávat zahájení.', zrus.historie, 'P18'
 FROM zrus;
 
-WITH zrus AS
-(
-    SELECT DISTINCT pr.ident_cely FROM projekt pr
-    JOIN historie h ON pr.historie = h.vazba
-    WHERE pr.stav = 1 AND h.typ_zmeny in ('PX0', 'PX1') AND h.datum_zmeny < (now() - INTERVAL '3 year') AND upper(pr.planovane_zahajeni) < (now() - interval '1 year')
+WITH zrus AS (
+	SELECT DISTINCT pr.ident_cely FROM projekt pr
+    		JOIN historie h ON pr.historie = h.vazba
+	WHERE pr.stav = 1 AND pr.typ_projektu = (SELECT id FROM heslar WHERE ident_cely = 'HES-001136') AND h.typ_zmeny in ('PX0', 'PX1') AND h.datum_zmeny < (now() - INTERVAL '3 year') AND upper(pr.planovane_zahajeni) < (now() - interval '1 year')
 )
 UPDATE projekt SET stav = 8 FROM zrus WHERE projekt.ident_cely = zrus.ident_cely;
 
+
 # Smazání os. údajů u dříve zrušených projektů
+WITH stare AS (
+	SELECT h1.vazba FROM historie h1	
+		JOIN soubor ON soubor.historie = h1.vazba
+		JOIN projekt ON projekt.soubory = soubor.vazba
+		JOIN historie h2 ON h2.vazba = projekt.historie
+	WHERE (projekt.stav = 8 AND h2.typ_zmeny = 'P78' AND h2.datum_zmeny < (now() - INTERVAL '1 year')) OR (projekt.stav = 8 AND h2.typ_zmeny = 'P18')
+)
+DELETE FROM historie WHERE historie.vazba IN (SELECT vazba FROM stare);
+	
 WITH stare AS (
 	SELECT projekt.soubory FROM projekt
 		JOIN historie ON historie.vazba = projekt.historie
@@ -29,7 +37,7 @@ WITH stare AS (
 	SELECT oznamovatel.projekt FROM oznamovatel
 		JOIN projekt ON oznamovatel.projekt = projekt.id
 		JOIN historie ON historie.vazba = projekt.historie
-	WHERE stav = 8 AND typ_zmeny = 'P78' AND datum_zmeny < (now() - INTERVAL '1 year')
+	WHERE stav = 8 AND typ_zmeny = 'P78' AND datum_zmeny < (now() - INTERVAL '1 year') OR (stav = 8 AND typ_zmeny = 'P18')
 )
 UPDATE oznamovatel SET
 	email = CURRENT_DATE || ': údaj odstraněn',
@@ -37,3 +45,29 @@ UPDATE oznamovatel SET
 	odpovedna_osoba = CURRENT_DATE || ': údaj odstraněn',
 	telefon = CURRENT_DATE || ': údaj odstraněn'
 FROM stare WHERE oznamovatel.projekt = stare.projekt;
+
+
+# Smazání oznamovatelů u velmi starých projektů
+WITH smaz AS (
+	SELECT h1.vazba FROM historie h1	
+		JOIN soubor ON soubor.historie = h1.vazba
+		JOIN projekt ON projekt.soubory = soubor.vazba
+		JOIN historie h2 ON h2.vazba = projekt.historie
+	WHERE h2.typ_zmeny IN ('PX0', 'PX1') AND typ_projektu = (SELECT id FROM heslar WHERE ident_cely = 'HES-001136') AND h2.datum_zmeny < (now() - INTERVAL '10 year')
+)
+DELETE FROM historie WHERE historie.vazba IN (SELECT vazba FROM smaz);
+	
+WITH smaz AS (
+	SELECT projekt.soubory FROM projekt
+		JOIN historie ON historie.vazba = projekt.historie
+	WHERE typ_zmeny IN ('PX0', 'PX1') AND typ_projektu = (SELECT id FROM heslar WHERE ident_cely = 'HES-001136') AND datum_zmeny < (now() - INTERVAL '10 year')
+)
+DELETE FROM soubor WHERE soubor.vazba IN (SELECT soubory FROM smaz);
+	
+WITH smaz AS (
+	SELECT oznamovatel.projekt FROM oznamovatel
+		JOIN projekt ON oznamovatel.projekt = projekt.id
+		JOIN historie ON historie.vazba = projekt.historie
+	WHERE typ_zmeny IN ('PX0', 'PX1') AND datum_zmeny < (now() - INTERVAL '10 year')
+)
+DELETE FROM oznamovatel WHERE oznamovatel.projekt IN (SELECT projekt FROM smaz);
