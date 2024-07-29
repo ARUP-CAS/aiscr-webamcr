7750c612b1ac4ad330b5c7b2baeb37bcd87dd0f1
David Novák <novak@arup.cas.cz>
2024-07-29 22:49:39 +0200

    Update views.py

--- webclient/dokument/views.py
+++ webclient/dokument/views.py
@@ -282,7 +282,6 @@ class Model3DListView(SearchListView):
         self.hasOnlyVybrat_header = _("dokument.views.Model3DListView.hasOnlyVybrat_header.text")
         self.hasOnlyVlastnik_header = _("dokument.views.Model3DListView.hasOnlyVlastnik_header.text")
         self.hasOnlyArchive_header = _("dokument.views.Model3DListView.hasOnlyArchive_header.text")
-        self.hasOnlyPotvrdit_header = _("dokument.views.Model3DListView.hasOnlyPotvrdit_header.text")
         self.hasOnlyNase_header = _("dokument.views.Model3DListView.hasOnlyNase_header.text")
         self.default_header = _("dokument.views.Model3DListView.default_header.text")
 
@@ -351,7 +350,6 @@ class DokumentListView(SearchListView):
         self.hasOnlyVybrat_header = _("dokument.views.DokumentListView.hasOnlyVybrat_header.text")
         self.hasOnlyVlastnik_header = _("dokument.views.DokumentListView.hasOnlyVlastnik_header.text")
         self.hasOnlyArchive_header = _("dokument.views.DokumentListView.hasOnlyArchive_header.text")
-        self.hasOnlyPotvrdit_header = _("dokument.views.DokumentListView.hasOnlyPotvrdit_header.text")
         self.hasOnlyNase_header = _("dokument.views.DokumentListView.hasOnlyNase_header.text")
         self.default_header = _("dokument.views.DokumentListView.default_header.text")
 
