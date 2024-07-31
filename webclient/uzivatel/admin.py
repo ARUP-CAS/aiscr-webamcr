2014459722af718c1f11a8b106fee2185c1df76a
Jan Ihnat <jnihnat@gmail.com>
2024-07-31 14:39:41 +0200

    initial hodnotu pre notifikace v adminu pokud je user vytvareny

--- webclient/uzivatel/admin.py
+++ webclient/uzivatel/admin.py
@@ -39,6 +39,22 @@ class UserNotificationTypeInlineForm(forms.ModelForm):
             |Q(ident_cely='E-U-04')
         )
 
+class UserNotificationTypeInlineFormset(forms.models.BaseInlineFormSet):
+    model = UserNotificationType.user.through
+
+    def __init__(self, *args, **kwargs):
+        super(UserNotificationTypeInlineFormset, self).__init__(*args, **kwargs)
+        logger.debug(self.data)
+        if not self.instance.pk and not self.data: 
+            notification_ids = UserNotificationType.objects.filter(
+                Q(ident_cely__icontains='S-E-')
+                |Q(ident_cely='E-U-04')
+            ).values_list('id', flat=True)
+            logger.debug(self.initial)
+            self.initial = []
+            for id in notification_ids:
+                self.initial.append({'usernotificationtype': id, })
+
 
 class UserNotificationTypeInline(admin.TabularInline):
     """
@@ -46,6 +62,7 @@ class UserNotificationTypeInline(admin.TabularInline):
     """
     model = UserNotificationType.user.through
     form = UserNotificationTypeInlineForm
+    formset = UserNotificationTypeInlineFormset
 
     def get_queryset(self, request):
         logger.debug(self.model._default_manager)
@@ -55,6 +72,14 @@ class UserNotificationTypeInline(admin.TabularInline):
             |Q(usernotificationtype__ident_cely='E-U-04')
         )
         return queryset
+    
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 1  #default 0
+        if not obj: #new create only
+            extra = UserNotificationType.objects.filter(
+                Q(ident_cely__icontains='S-E-')
+                |Q(ident_cely='E-U-04')).count()
+        return extra
 
     def __init__(self, parent_model, admin_site):
         super(UserNotificationTypeInline, self).__init__(parent_model, admin_site)
