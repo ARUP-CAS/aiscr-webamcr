6f853dcb505ad9260d516268a40f39d050214260
Jan Havrlant <jhavrlant@arup.cas.cz>
2024-07-31 18:57:35 +0200

    #2209 Oprava předávání IP adresy proxy serveru

--- scripts/build_prod_image.sh
+++ scripts/build_prod_image.sh
@@ -3,3 +3,4 @@
 
 #Wrapper for building production image locally
 docker build -t test_prod -f Dockerfile-production --build-arg VERSION_APP="$(git rev-parse --short HEAD | head -c 8)" --build-arg TAG_APP=local_build  .
+docker build -t test_proxy -f proxy/Dockerfile --build-arg VERSION_APP="$(git rev-parse --short HEAD | head -c 8)" --build-arg TAG_APP=local_build  ./proxy
