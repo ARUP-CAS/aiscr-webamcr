bcb8d32c2b93b57ce3d4e58bef6a12a1b9781f94
Jan Havrlant <jhavrlant@arup.cas.cz>
2024-08-03 08:43:56 +0200

    Oprava

--- webclient/static/js/dz.js
+++ webclient/static/js/dz.js
@@ -131,6 +131,7 @@ window.onload = function () {
             "application/vnd.rar, " +
             "application/x-rar, " +
             "application/x-rar-compressed, " +
+            "application/x-compressed," +
             "application/x-7z-compressed"
         RejectedFileMessage = reject_dict["rejected_dokument_model"]
     } else {
