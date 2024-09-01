7d21d20bfc2017589ea7c9bb7a6827b4a4baf3bf
jnihnat <57906106+jnihnat@users.noreply.github.com>
2024-09-01 21:50:04 +0200

    Pridany proklik na projekt u PAS pro C-E (#2280)

--- webclient/pas/views.py
+++ webclient/pas/views.py
@@ -1044,6 +1044,7 @@ def get_detail_template_shows(sn, user):
         "soubor_nahled": check_permissions(p.actionChoices.soubor_nahled_pas, user, sn.ident_cely),
         "soubor_smazat": check_permissions(p.actionChoices.soubor_smazat_pas, user, sn.ident_cely),
         "soubor_nahradit": check_permissions(p.actionChoices.soubor_nahradit_pas, user, sn.ident_cely),
+        "backtoprojekt": user.is_archeolog_or_more
     }
     return show
 
