3d8eb842d03899fa58b1051ff73fc08a17dbad2d
Jan Ihnat <jnihnat@gmail.com>
2024-09-09 22:25:09 +0200

    Pridani extra radku pro nalezy u dokumentu a akci

--- webclient/nalez/views.py
+++ webclient/nalez/views.py
@@ -8,6 +8,7 @@ from core.message_constants import (
     ZAZNAM_USPESNE_EDITOVAN,
     ZAZNAM_USPESNE_SMAZAN,
 )
+from core.repository_connector import FedoraTransaction
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -18,8 +19,6 @@ from django.urls import reverse
 from django.utils.http import url_has_allowed_host_and_scheme
 from django.utils.translation import gettext as _
 from django.views.decorators.http import require_http_methods
-
-from core.repository_connector import FedoraTransaction
 from dokument.models import Dokument
 from heslar.hesla import (
     HESLAR_OBJEKT_DRUH,
@@ -30,7 +29,6 @@ from heslar.hesla import (
     HESLAR_PREDMET_DRUH_KAT,
     HESLAR_PREDMET_SPECIFIKACE,
 )
-from heslar.models import Heslar
 from heslar.views import heslar_12, heslar_list
 from komponenta.models import Komponenta
 from nalez.forms import create_nalez_objekt_form, create_nalez_predmet_form
@@ -99,52 +97,48 @@ def edit_nalez(request, typ_vazby, komp_ident_cely):
     """
     komponenta: Komponenta = get_object_or_404(Komponenta, ident_cely=komp_ident_cely)
     druh_objekt_choices = heslar_12(HESLAR_OBJEKT_DRUH, HESLAR_OBJEKT_DRUH_KAT)
-    specifikace_objekt_choices = heslar_12(
-        HESLAR_OBJEKT_SPECIFIKACE, HESLAR_OBJEKT_SPECIFIKACE_KAT
-    )
+    specifikace_objekt_choices = heslar_12(HESLAR_OBJEKT_SPECIFIKACE, HESLAR_OBJEKT_SPECIFIKACE_KAT)
     NalezObjektFormset = inlineformset_factory(
         Komponenta,
         NalezObjekt,
         form=create_nalez_objekt_form(druh_objekt_choices, specifikace_objekt_choices),
-        extra=1,
+        extra=3,
     )
-    formset_objekt = NalezObjektFormset(
-        request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_o"
-    )
+    formset_objekt = NalezObjektFormset(request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_o")
 
     druh_predmet_choices = heslar_12(HESLAR_PREDMET_DRUH, HESLAR_PREDMET_DRUH_KAT)
     specifikce_predmetu_choices = heslar_list(HESLAR_PREDMET_SPECIFIKACE)
     NalezPredmetFormset = inlineformset_factory(
         Komponenta,
         NalezPredmet,
-        form=create_nalez_predmet_form(
-            druh_predmet_choices, specifikce_predmetu_choices
-        ),
-        extra=1,
+        form=create_nalez_predmet_form(druh_predmet_choices, specifikce_predmetu_choices),
+        extra=3,
     )
-    formset_predmet = NalezPredmetFormset(
-        request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_p"
-    )
+    formset_predmet = NalezPredmetFormset(request.POST, instance=komponenta, prefix=komponenta.ident_cely + "_p")
     if formset_objekt.is_valid() and formset_predmet.is_valid():
-        logger.debug("nalez.views.edit_nalez.form_valid",
-                     extra={"typ_vazby": komponenta.komponenta_vazby.typ_vazby})
+        logger.debug("nalez.views.edit_nalez.form_valid", extra={"typ_vazby": komponenta.komponenta_vazby.typ_vazby})
         formset_predmet.save()
         formset_objekt.save()
         if formset_objekt.has_changed() or formset_predmet.has_changed():
             if komponenta.komponenta_vazby.typ_vazby == DOKUMENT_CAST_RELATION_TYPE:
                 navazany_objekt: Dokument = komponenta.komponenta_vazby.casti_dokumentu
                 navazany_objekt.active_transaction = FedoraTransaction()
-                logger.debug("nalez.views.edit_nalez.form_valid.save_metadata_dokument",
-                             extra={"ident_cely": navazany_objekt.ident_cely,
-                                    "fedora_transaction": navazany_objekt.active_transaction.uid})
+                logger.debug(
+                    "nalez.views.edit_nalez.form_valid.save_metadata_dokument",
+                    extra={
+                        "ident_cely": navazany_objekt.ident_cely,
+                        "fedora_transaction": navazany_objekt.active_transaction.uid,
+                    },
+                )
                 navazany_objekt.close_active_transaction_when_finished = True
                 navazany_objekt.save()
             logger.debug("Form data was changed")
             messages.add_message(request, messages.SUCCESS, ZAZNAM_USPESNE_EDITOVAN)
     else:
-        logger.debug("nalez.views.edit_nalez.form_not_valid",
-                     extra={"formset_predmet_errors": formset_predmet.errors,
-                            "formset_objekt_errors": formset_objekt.errors})
+        logger.debug(
+            "nalez.views.edit_nalez.form_not_valid",
+            extra={"formset_predmet_errors": formset_predmet.errors, "formset_objekt_errors": formset_objekt.errors},
+        )
         messages.add_message(request, messages.ERROR, ZAZNAM_SE_NEPOVEDLO_EDITOVAT)
         request.session["_old_nalez_post"] = request.POST
         request.session["komp_ident_cely"] = komp_ident_cely
@@ -153,10 +147,6 @@ def edit_nalez(request, typ_vazby, komp_ident_cely):
     else:
         safe_redirect = "/"
     response = redirect(safe_redirect)
-    response.set_cookie(
-        "show-form", f"detail_komponenta_form_{komp_ident_cely}", max_age=1000
-    )
-    response.set_cookie(
-        "set-active", f"el_komponenta_{komp_ident_cely.replace('-', '_')}", max_age=1000
-    )
+    response.set_cookie("show-form", f"detail_komponenta_form_{komp_ident_cely}", max_age=1000)
+    response.set_cookie("set-active", f"el_komponenta_{komp_ident_cely.replace('-', '_')}", max_age=1000)
     return response
