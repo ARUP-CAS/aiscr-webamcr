04b7b2b5c574ce01a674184a7c868cf2241f13b4
jnihnat <57906106+jnihnat@users.noreply.github.com>
2024-09-12 15:52:03 +0200

    Oprava pro dokument format (#2340)
    Co-authored-by: Jiří Pešík <jiri.pesik@outlook.cz>

--- webclient/heslar/views.py
+++ webclient/heslar/views.py
@@ -1,17 +1,16 @@
+import logging
+
 from dal import autocomplete
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.gis.geos import Point
-from django.db import OperationalError, ProgrammingError
-from django.http import JsonResponse
-from django.db.models import Value, IntegerField
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import OperationalError, ProgrammingError
+from django.db.models import IntegerField, Value
+from django.http import JsonResponse
 from django.utils.translation import get_language
-
-from heslar.hesla import HESLAR_DOKUMENT_TYP, HESLAR_DOKUMENT_FORMAT, HESLAR_PRISTUPNOST
-from heslar.hesla_dynamicka import MODEL_3D_DOKUMENT_TYPES, MODEL_3D_DOKUMENT_FORMATS
-from heslar.models import Heslar, HeslarNazev, RuianKatastr, HeslarHierarchie
-import logging
-
+from heslar.hesla import HESLAR_DOKUMENT_FORMAT, HESLAR_DOKUMENT_TYP, HESLAR_PRISTUPNOST
+from heslar.hesla_dynamicka import MODEL_3D_DOKUMENT_FORMATS, MODEL_3D_DOKUMENT_TYPES
+from heslar.models import Heslar, HeslarHierarchie, HeslarNazev, RuianKatastr
 
 logger = logging.getLogger(__name__)
 
@@ -20,12 +19,17 @@ class RuianKatastrAutocomplete(autocomplete.Select2QuerySetView):
     """
     Třída pohledu pro autocomplete ruian katastru.
     """
+
     def get_queryset(self):
         qs = RuianKatastr.objects.all()
         if self.q:
             new_qs = qs.filter(nazev__istartswith=self.q).annotate(qs_order=Value(0, IntegerField()))
-            new_qs2 =qs.filter(nazev__icontains=self.q).exclude(nazev__istartswith=self.q).annotate(qs_order=Value(2, IntegerField()))
-            qs = new_qs.union(new_qs2).order_by("qs_order","nazev")
+            new_qs2 = (
+                qs.filter(nazev__icontains=self.q)
+                .exclude(nazev__istartswith=self.q)
+                .annotate(qs_order=Value(2, IntegerField()))
+            )
+            qs = new_qs.union(new_qs2).order_by("qs_order", "nazev")
         return qs
 
 
@@ -34,7 +38,7 @@ def merge_heslare(first, second):
     Pomocní funkce pro vytvoření dvoustupňového selectu.
     """
     data = [("", "")]
-    #logger.debug(get_language())
+    # logger.debug(get_language())
     try:
         for k in first:
             druhy_kategorie = []
@@ -57,20 +61,20 @@ def merge_heslare(first, second):
     return data
 
 
-def heslar_12(druha, prvni_kat,id=False):
+def heslar_12(druha, prvni_kat, id=False):
     """
     Funkce pro vytvoření dvoustupňového selectu.
     """
     druha = (
         Heslar.objects.filter(nazev_heslare=druha)
         .order_by("razeni")
-        .values("id", "hierarchie__heslo_nadrazene", "heslo","heslo_en")
+        .values("id", "hierarchie__heslo_nadrazene", "heslo", "heslo_en")
     )
     if id:
-        kategorie=Heslar.objects.filter(nazev_heslare=prvni_kat,id__in=id)
+        kategorie = Heslar.objects.filter(nazev_heslare=prvni_kat, id__in=id)
     else:
-        kategorie=Heslar.objects.filter(nazev_heslare=prvni_kat)
-    prvni = kategorie.order_by("razeni").values("id", "heslo","heslo_en")
+        kategorie = Heslar.objects.filter(nazev_heslare=prvni_kat)
+    prvni = kategorie.order_by("razeni").values("id", "heslo", "heslo_en")
     return merge_heslare(prvni, druha)
 
 
@@ -79,9 +83,7 @@ def zjisti_katastr_souradnic(request):
     Funkce pohledu pro vrácení katastru podle souradnic.
     """
     nalezene_katastry = RuianKatastr.objects.filter(
-        hranice__contains=Point(
-            float(request.GET.get("long", 0)), float(request.GET.get("lat", 0))
-        )
+        hranice__contains=Point(float(request.GET.get("long", 0)), float(request.GET.get("lat", 0)))
     )
     if nalezene_katastry.count() == 1:
         return JsonResponse(
@@ -93,6 +95,7 @@ def zjisti_katastr_souradnic(request):
     else:
         return JsonResponse({})
 
+
 def zjisti_vychozi_hodnotu(request):
     """
     Funkce pohledu pro zjištení výchozí hodnoty z heslaře.
@@ -100,17 +103,13 @@ def zjisti_vychozi_hodnotu(request):
     nadrazene = request.GET.get("nadrazene", 0)
     vychozi_hodnota = HeslarHierarchie.objects.filter(heslo_nadrazene=nadrazene, typ="výchozí hodnota")
     if vychozi_hodnota.exists():
-        queryset = vychozi_hodnota.values_list('heslo_podrazene', flat=True)
+        queryset = vychozi_hodnota.values_list("heslo_podrazene", flat=True)
         list = []
         for id in queryset:
-            list.append({"id":id})
-        return JsonResponse(
-            data=list,
-            status=200,
-            safe=False
-        )
+            list.append({"id": id})
+        return JsonResponse(data=list, status=200, safe=False)
     else:
-        return JsonResponse(data = {},status=400)
+        return JsonResponse(data={}, status=400)
 
 
 def zjisti_nadrazenou_hodnotu(request):
@@ -128,20 +127,16 @@ def zjisti_nadrazenou_hodnotu(request):
             logger.debug("heslar.views.zjisti_nadrazenou_hodnotu.does_not_exist", extra={"err": err})
             return JsonResponse(data={}, status=400)
     list = [{"id": nadrazene.id}]
-    return JsonResponse(
-        data=list,
-        status=200,
-        safe=False
-    )
+    return JsonResponse(data=list, status=200, safe=False)
 
 
 class DokumentTypAutocomplete(LoginRequiredMixin, autocomplete.Select2QuerySetView):
     """
     Třída pohledu pro autocomplete dokument typu.
     """
+
     def get_queryset(self):
-        qs = Heslar.objects.filter(nazev_heslare=HESLAR_DOKUMENT_TYP) \
-        .filter(id__in=MODEL_3D_DOKUMENT_TYPES)
+        qs = Heslar.objects.filter(nazev_heslare=HESLAR_DOKUMENT_TYP).filter(id__in=MODEL_3D_DOKUMENT_TYPES)
         if self.q:
             qs = qs.filter(nazev__icontains=self.q)
         return qs
@@ -151,9 +146,9 @@ class DokumentFormatAutocomplete(LoginRequiredMixin, autocomplete.Select2QuerySe
     """
     Třída pohledu pro autocomplete dokument formatu.
     """
+
     def get_queryset(self):
-        qs = Heslar.objects.filter(nazev_heslare=HESLAR_DOKUMENT_FORMAT) \
-        .filter(id__in=MODEL_3D_DOKUMENT_FORMATS)
+        qs = Heslar.objects.filter(nazev_heslare=HESLAR_DOKUMENT_FORMAT).filter(id__in=MODEL_3D_DOKUMENT_FORMATS)
         if self.q:
             qs = qs.filter(nazev__icontains=self.q)
         return qs
@@ -163,47 +158,48 @@ class PristupnostAutocomplete(LoginRequiredMixin, autocomplete.Select2QuerySetVi
     """
     Třída pohledu pro autocomplete pristupnosti.
     """
+
     def get_queryset(self):
         qs = Heslar.objects.filter(nazev_heslare=HESLAR_PRISTUPNOST)
         if self.q:
             qs = qs.filter(nazev__icontains=self.q)
         return qs
 
+
 class HeslarAutocompleteView(LoginRequiredMixin, autocomplete.Select2QuerySetView):
     """
     Třída pohledu pro autocomplete pristupnosti.
     """
+
     def get_queryset(self):
         qs = Heslar.objects.all()
-        heslar_nazev = self.forwarded.get('heslar_nazev', None)
+        heslar_nazev = self.forwarded.get("heslar_nazev", None)
         if self.q:
             qs = qs.filter(heslo__icontains=self.q)
         if heslar_nazev:
             qs = qs.filter(nazev_heslare=heslar_nazev)
         return qs
-    
+
+
 class HeslarNazevAutocompleteView(LoginRequiredMixin, autocomplete.Select2QuerySetView):
     """
     Třída pohledu pro autocomplete pristupnosti.
     """
+
     def get_queryset(self):
         qs = HeslarNazev.objects.all()
         if self.q:
             qs = qs.filter(nazev__icontains=self.q)
         return qs
 
-def heslar_list(heslo_nazev,filter={}):
+
+def heslar_list(heslo_nazev, filter={}, use_exclude=False):
     hesla = Heslar.objects.filter(nazev_heslare=heslo_nazev)
-    hesla_filtered = hesla.filter(**filter)
-    if get_language() == "en":
-        return list(
-            hesla_filtered.values_list(
-                "id", "heslo_en"
-            )
-        )
+    if use_exclude:
+        hesla_filtered = hesla.exclude(**filter)
     else:
-        return list(
-            hesla_filtered.values_list(
-                "id", "heslo"
-            )
-        )
+        hesla_filtered = hesla.filter(**filter)
+    if get_language() == "en":
+        return list(hesla_filtered.values_list("id", "heslo_en"))
+    else:
+        return list(hesla_filtered.values_list("id", "heslo"))
