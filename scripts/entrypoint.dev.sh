5b141f690470bef20f393494252b42394e9bd838
Jan Ihnat <jnihnat@gmail.com>
2024-11-06 21:55:13 +0100

    oprava na python3

--- scripts/entrypoint.dev.sh
+++ scripts/entrypoint.dev.sh
@@ -2,5 +2,5 @@
 
 python3 manage.py migrate
 #python3 manage.py runserver 0.0.0.0:8000
-python manage.py runserver_plus --cert-file /cert/amcr-test-local.crt 0.0.0.0:8000
+python3 manage.py runserver_plus --cert-file /cert/amcr-test-local.crt 0.0.0.0:8000
 python3 manage.py shell < data_management.py
