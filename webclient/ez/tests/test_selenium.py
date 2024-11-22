24067ec2cd3df26a9c8e431e476cc7bf34c63b15
Jan Havrlant <jhavrlant@arup.cas.cz>
2024-11-22 10:30:40 +0100

    test 120-125

--- webclient/ez/tests/test_selenium.py
+++ webclient/ez/tests/test_selenium.py
@@ -2,7 +2,7 @@ import logging
 import unittest
 
 from arch_z.models import ArcheologickyZaznam, ExterniOdkaz
-from core.constants import EZ_STAV_ODESLANY, EZ_STAV_ZAPSANY
+from core.constants import EZ_STAV_ODESLANY, EZ_STAV_POTVRZENY, EZ_STAV_ZAPSANY
 from core.tests.test_selenium import BaseSeleniumTestClass, WaitForPageLoad
 from django.conf import settings
 from django.utils.translation import gettext as _
@@ -70,22 +70,138 @@ class AkceExterniZdroj(BaseSeleniumTestClass):
         logger.info("AkceExterniZdroj.test_119_pripojeni_akce_externího_zdroje_p_001.start")
         self.login("archeolog")
         self.createFedoraRecord("X-BIB-000000001")
-
         id = ExterniZdroj.objects.filter(ident_cely="X-BIB-000000001").first().id
         ez_odkazy = ExterniOdkaz.objects.filter(externi_zdroj=id)
         count_old = ez_odkazy.filter(archeologicky_zaznam__typ_zaznamu=ArcheologickyZaznam.TYP_ZAZNAMU_AKCE).count()
         self.goToAddress("/id/X-BIB-000000001")
-
         self.ElementClick(By.ID, "eo-pripojit-akce")
-
         self.ElementClick(By.ID, "select2-id_arch_z-container")
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys("X-C-9000000001A")
         self.wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(Keys.ENTER)
-
         with WaitForPageLoad(self.driver):
             self.ElementClick(By.ID, "submit-btn")
         ez_odkazy = ExterniOdkaz.objects.filter(externi_zdroj=id)
         count_new = ez_odkazy.filter(archeologicky_zaznam__typ_zaznamu=ArcheologickyZaznam.TYP_ZAZNAMU_AKCE).count()
         self.assertEqual(count_old + 1, count_new)
         logger.info("AkceExterniZdroj.test_119_pripojeni_akce_externího_zdroje_p_001.end")
+
+    def test_120_pripojeni_lokality_externího_zdroje_p_001(self):
+        # Scenar_120 Připojení lokality k externímu zdroji (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_120_pripojeni_lokality_externího_zdroje_p_001.start")
+        self.login("archeolog")
+        self.createFedoraRecord("X-BIB-000000001")
+        id = ExterniZdroj.objects.filter(ident_cely="X-BIB-000000001").first().id
+        ez_odkazy = ExterniOdkaz.objects.filter(externi_zdroj=id)
+        count_old = ez_odkazy.filter(archeologicky_zaznam__typ_zaznamu=ArcheologickyZaznam.TYP_ZAZNAMU_LOKALITA).count()
+        self.goToAddress("/id/X-BIB-000000001")
+        self.ElementClick(By.ID, "eo-pripojit-lokalita")
+        self.ElementClick(By.ID, "select2-id_arch_z-container")
+        self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys("C-K9000001")
+        self.wait(2)
+        self.driver.find_element(By.CSS_SELECTOR, ".select2-search__field").send_keys(Keys.ENTER)
+        with WaitForPageLoad(self.driver):
+            self.ElementClick(By.ID, "submit-btn")
+        ez_odkazy = ExterniOdkaz.objects.filter(externi_zdroj=id)
+        count_new = ez_odkazy.filter(archeologicky_zaznam__typ_zaznamu=ArcheologickyZaznam.TYP_ZAZNAMU_LOKALITA).count()
+        self.assertEqual(count_old + 1, count_new)
+        logger.info("AkceExterniZdroj.test_120_pripojeni_lokality_externího_zdroje_p_001.end")
+
+    def test_121_potvrzení_externího_zdroje_p_001(self):
+        # Scenar_121 Potvrzení externího zdroje (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_121_potvrzení_externího_zdroje_p_001.start")
+        self.login("archivar")
+        self.createFedoraRecord("X-BIB-1408662")
+        dbID = ExterniZdroj.objects.filter(ident_cely="X-BIB-1408662").first().id
+        self.assertEqual(ExterniZdroj.objects.filter(id=dbID).first().stav, EZ_STAV_ODESLANY)
+        self.goToAddress("/id/X-BIB-1408662")
+        self.ElementClick(By.ID, "ez-potvrdit")
+        with WaitForPageLoad(self.driver):
+            self.ElementClick(By.ID, "submit-btn")
+        self.assertEqual(ExterniZdroj.objects.filter(id=dbID).first().stav, EZ_STAV_POTVRZENY)
+        logger.info("AkceExterniZdroj.test_121_potvrzení_externího_zdroje_p_001.end")
+
+    def test_122_zapsani_externího_zdroje_p_002(self):
+        # Scenar_122 Zapsání nového externího zdroje (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_122_zapsani_externího_zdroje_p_002.start")
+        self.login("badatel")
+        count_old = ExterniZdroj.objects.count()
+        self.zaspat_zaznam()
+        count_new = ExterniZdroj.objects.count()
+        self.assertEqual(count_old + 1, count_new)
+        logger.info("AkceExterniZdroj.test_122_zapsani_externího_zdroje_p_002.end")
+
+    def test_123_odeslani_externího_zdroje_p_001(self):
+        # Scenar_118 Odeslání záznamu Externí zdroj (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_123_odeslani_externího_zdroje_p_001.start")
+        self.login("badatel")
+        self.createFedoraRecord("X-BIB-000000002")
+        self.assertEqual(ExterniZdroj.objects.filter(ident_cely="X-BIB-000000002").first().stav, EZ_STAV_ZAPSANY)
+        id = ExterniZdroj.objects.filter(ident_cely="X-BIB-000000002").first().id
+        self.goToAddress("/id/X-BIB-000000002")
+        self.ElementClick(By.ID, "ez-odeslat")
+        with WaitForPageLoad(self.driver):
+            self.ElementClick(By.ID, "submit-btn")
+        self.assertEqual(ExterniZdroj.objects.filter(id=id).first().stav, EZ_STAV_ODESLANY)
+        logger.info("AkceExterniZdroj.test_123_odeslani_externího_zdroje_p_001.end")
+
+    def test_124_zapsani_externího_zdroje_p_003(self):
+        # Scenar_124 Zápsání nového externího zdroje typu část knihy (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_124_zapsani_externího_zdroje_p_003.start")
+        self.login("archeolog")
+        count_old = ExterniZdroj.objects.count()
+        self.go_to_form_zapsat()
+        self.ElementClick(By.CSS_SELECTOR, ".required-next > .btn")
+        self.ElementClick(By.CSS_SELECTOR, "#bs-select-1-1 > .text")
+        self.ElementClick(By.CSS_SELECTOR, "#div_id_autori .select2-selection__rendered")
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_autori .select2-search__field").send_keys("havrlant")
+        self.wait(1)
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_autori .select2-search__field").send_keys(Keys.ENTER)
+        self.ElementClick(By.CSS_SELECTOR, "#div_id_editori .select2-selection__rendered")
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_editori .select2-search__field").send_keys("abr")
+        self.wait(1)
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_editori .select2-search__field").send_keys(Keys.ENTER)
+        self.ElementClick(By.ID, "id_rok_vydani_vzniku")
+        self.driver.find_element(By.ID, "id_rok_vydani_vzniku").send_keys("2023")
+        self.driver.find_element(By.ID, "id_rok_vydani_vzniku").send_keys(Keys.ENTER)
+        self.driver.find_element(By.ID, "id_nazev").send_keys("test")
+        self.ElementClick(By.ID, "id_paginace_titulu")
+        self.driver.find_element(By.ID, "id_paginace_titulu").send_keys("test1")
+        self.ElementClick(By.ID, "id_sbornik_nazev")
+        self.driver.find_element(By.ID, "id_sbornik_nazev").send_keys("Sborník")
+        self.ElementClick(By.ID, "id_misto")
+        self.driver.find_element(By.ID, "id_misto").send_keys("Praha")
+        with WaitForPageLoad(self.driver):
+            self.ElementClick(By.ID, "newEntitySubmitBtn")
+        count_new = ExterniZdroj.objects.count()
+        self.assertEqual(count_old + 1, count_new)
+        logger.info("AkceExterniZdroj.test_124_zapsani_externího_zdroje_p_003.end")
+
+    def test_125_zapsani_externího_zdroje_p_004(self):
+        # Scenar_125 Zapsání nového externího zdroje typu článek v časopise (pozitivní scénář 1)
+        logger.info("AkceExterniZdroj.test_125_zapsani_externího_zdroje_p_004.start")
+        self.login("archeolog")
+        count_old = ExterniZdroj.objects.count()
+        self.go_to_form_zapsat()
+        self.ElementClick(By.CSS_SELECTOR, ".required-next > .btn")
+        self.ElementClick(By.CSS_SELECTOR, "#bs-select-1-2 > .text")
+        self.ElementClick(By.CSS_SELECTOR, "#div_id_autori .select2-selection__rendered")
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_autori .select2-search__field").send_keys("havrlant")
+        self.wait(1)
+        self.driver.find_element(By.CSS_SELECTOR, "#div_id_autori .select2-search__field").send_keys(Keys.ENTER)
+        self.ElementClick(By.ID, "id_rok_vydani_vzniku")
+        self.driver.find_element(By.ID, "id_rok_vydani_vzniku").send_keys("2024")
+        self.driver.find_element(By.ID, "id_rok_vydani_vzniku").send_keys(Keys.ENTER)
+        self.ElementClick(By.ID, "id_nazev")
+        self.driver.find_element(By.ID, "id_nazev").send_keys("test")
+        self.ElementClick(By.ID, "id_casopis_denik_nazev")
+        self.driver.find_element(By.ID, "id_casopis_denik_nazev").send_keys("nevim")
+        self.ElementClick(By.ID, "id_casopis_rocnik")
+        self.driver.find_element(By.ID, "id_casopis_rocnik").send_keys("2024")
+        self.ElementClick(By.ID, "id_paginace_titulu")
+        self.driver.find_element(By.ID, "id_paginace_titulu").send_keys("test1")
+        with WaitForPageLoad(self.driver):
+            self.ElementClick(By.ID, "newEntitySubmitBtn")
+        count_new = ExterniZdroj.objects.count()
+        self.assertEqual(count_old + 1, count_new)
+        logger.info("AkceExterniZdroj.test_125_zapsani_externího_zdroje_p_004.end")
