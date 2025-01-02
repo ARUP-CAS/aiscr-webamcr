7be72c3ad7502849b53fe911d1b99fd4b4ded925
Jan Havrlant <155644357+jhavrlant@users.noreply.github.com>
2025-01-02 22:29:50 +0100

    Načítání hesel z databáze (#2676)

---
+++ webclient/core/setting_models.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_prometheus.models import ExportModelOperationsMixin
+
+logger = logging.getLogger(__name__)
+
+
+class CustomAdminSettings(ExportModelOperationsMixin("custom_admin_settings"), models.Model):
+    item_group = models.CharField(max_length=100)
+    item_id = models.CharField(max_length=100)
+    value = models.TextField()
+
+    class Meta:
+        verbose_name = _("core.model.CustomAdminSettings.modelTitle.label")
+        verbose_name_plural = _("core.model.CustomAdminSettings.modelTitles.label")
