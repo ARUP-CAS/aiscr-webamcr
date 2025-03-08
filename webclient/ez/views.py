d7bd37d97303d001fac435c77827f77830b068b7
Jiří Pešík <jiri.pesik@outlook.cz>
2025-03-08 13:57:16 +0100

    Bugfix #2859

--- webclient/ez/views.py
+++ webclient/ez/views.py
@@ -536,6 +536,12 @@ class ExterniOdkazPripojitView(TransakceView):
             eo.active_transaction = self.active_transaction
             eo.close_active_transaction_when_finished = True
             eo.save()
+            if (
+                arch_z.typ_zaznamu == ArcheologickyZaznam.TYP_ZAZNAMU_LOKALITA
+                and arch_z.stav == AZ_STAV_ARCHIVOVANY
+                and arch_z.lokalita.igsn
+            ):
+                arch_z.lokalita.igsn_update()
         else:
             logger.debug("ez.views.ExterniOdkazPripojitView.post.form_error", extra={"form_errors": form.errors})
         return JsonResponse({"redirect": ez.get_absolute_url()})
@@ -635,6 +641,12 @@ class ExterniOdkazOdpojitAZView(TransakceView):
             logger.debug("Externi odkaz - Archeologicky zaznam wrong relation")
             messages.add_message(request, messages.ERROR, SPATNY_ZAZNAM_ZAZNAM_VAZBA)
             return JsonResponse({"redirect": self.get_zaznam().get_absolute_url()}, status=403)
+        if (
+            eo.arch_z.typ_zaznamu == ArcheologickyZaznam.TYP_ZAZNAMU_LOKALITA
+            and eo.arch_z.stav == AZ_STAV_ARCHIVOVANY
+            and eo.arch_z.lokalita.igsn
+        ):
+            eo.arch_z.lokalita.igsn_update()
         return super().dispatch(request, *args, **kwargs)
 
     def get_zaznam(self):
