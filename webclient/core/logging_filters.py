c4ea8aa4c0b6ac7ab4b03d5899352f71d4e60d05
Jan Havrlant <jhavrlant@arup.cas.cz>
2025-03-17 08:20:10 +0100

    Oprava kruhového importu

--- webclient/core/logging_filters.py
+++ webclient/core/logging_filters.py
@@ -3,7 +3,7 @@ import logging
 
 class UserLogFilter(logging.Filter):
     def filter(self, record):
-        from core.middleware import LogMiddleware
+        from core.log_middleware import LogMiddleware
 
         record.url = LogMiddleware.get_request_url()
         record.user_id = LogMiddleware.get_user_id() or "Anonymous"
--- webclient/core/middleware.py
+++ webclient/core/middleware.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import threading
 from datetime import datetime, timedelta
 
 from core.connectors import RedisConnector
@@ -145,25 +144,3 @@ class StatusMessageMiddleware:
             if status:
                 self._show_message(status, request, redis_key)
                 break
-
-
-log_request_data = threading.local()
-
-
-class LogMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        log_request_data.url = request.path
-        log_request_data.user_id = request.user.ident_cely if request.user.is_authenticated else None
-        response = self.get_response(request)
-        return response
-
-    @staticmethod
-    def get_request_url():
-        return getattr(log_request_data, "url", None)
-
-    @staticmethod
-    def get_user_id():
-        return getattr(log_request_data, "user_id", None)
