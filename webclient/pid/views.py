a366c73d95a93afcfa17a3ab4f43fa4a3b1e4e64
Jiří Pešík <jiri.pesik@outlook.cz>
2025-04-20 16:31:34 +0200

    Added query to cross ref #2921

--- webclient/pid/views.py
+++ webclient/pid/views.py
@@ -60,7 +60,7 @@ class DoiAutocompleteView(LoginRequiredMixin, ApiView):
     CACHE_PREFIX = "DOI"
 
     @classmethod
-    def api_call(cls, q, use_cache=False):
+    def _api_call_data_cite(cls, q):
         params = {
             "query": f"doi:*{q.upper()}*",
         }
@@ -73,6 +73,40 @@ class DoiAutocompleteView(LoginRequiredMixin, ApiView):
                 title = record.get("attributes").get("titles")[0].get("title") or record.get("id")
                 id = record.get("id")
                 results.append([id, f"{title} ({id})"])
+        return results
+
+    @classmethod
+    def _api_call_cross_ref_doi(cls, q):
+        base_url = f"https://api.crossref.org/works/{q}"
+        response = requests.get(base_url)
+        if response.status_code == 200:
+            response = response.json()
+            if response.get("message").get("title"):
+                title = response.get("message").get("title")[0]
+            else:
+                title = response.get("message").get("DOI")
+            id = response.get("message").get("DOI")
+            return [[id, f"{title} ({id})"]]
+
+    @classmethod
+    def _api_call_cross_ref_title(cls, q):
+        base_url = f"https://api.crossref.org/works"
+        params = {"query.title": q}
+        response = requests.get(base_url, params=params)
+        results = []
+        if response.status_code == 200:
+            response = response.json()
+            for record in response.get("message").get("items", []):
+                title = record.get("title")[0] if record.get("title") else record.get("id")
+                id = record.get("DOI")
+                results.append([id, f"{title} ({id})"])
+        return results
+
+    @classmethod
+    def api_call(cls, q, use_cache=False):
+        results = cls._api_call_cross_ref_doi(q)
+        if not results:
+            results = cls._api_call_data_cite(q) + cls._api_call_cross_ref_title(q)
         return results
 
 
