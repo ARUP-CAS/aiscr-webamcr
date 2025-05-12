cfb80bb47d5d171debfe013ead04fb44498c7040
Jiří Pešík <jiri.pesik@outlook.cz>
2025-05-12 17:50:17 +0200

    Code review #3005

--- webclient/pid/exceptions.py
+++ webclient/pid/exceptions.py
@@ -6,7 +6,6 @@ class DoiWriteError(Exception):
     def __init__(self, status_code=None, response_text=None, request_url=None):
         message = f"Request to {request_url} failed with status {status_code} and response: {response_text}."
         super().__init__(message)
-        super().__init__()
         self.status_code = status_code
         self.response_text = response_text
         self.request_url = request_url
