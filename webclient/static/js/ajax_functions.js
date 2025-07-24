6227e1bd9022b4b005e8316b7e82d1b96653c5d5
Jan Havrlant <jhavrlant@arup.cas.cz>
2025-07-24 15:30:10 +0200

    Přesun get_vychozi_hodnota_podrazeneho

---
+++ webclient/static/js/ajax_functions.js
@@ -0,0 +1,23 @@
+get_vychozi_hodnota_podrazeneho = function (nadrazeneID, podrazeneID, start_url) {
+    $(nadrazeneID).on("changed.bs.select", 
+      function(e, clickedIndex, newValue, oldValue) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      if (this.status == 200){
+        var array = []
+        for (var i = 0; i < JSON.parse(this.responseText).length; i++) {
+          array = array.concat(JSON.parse(this.responseText)[i].id) 
+        }
+        $(podrazeneID).selectpicker('val', array);
+        $(podrazeneID).selectpicker('refresh');
+      }
+      else {
+        $(podrazeneID).selectpicker('val', "");
+        $(podrazeneID).selectpicker('refresh');
+      }
+    }
+    next_url = start_url + this.value
+    xhttp.open("GET", next_url);
+    xhttp.send();
+    });
+    }
