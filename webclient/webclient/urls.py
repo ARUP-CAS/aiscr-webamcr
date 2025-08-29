266ad432313e0b8364d3e3ca80a922d24fb4c0f1
Jan Havrlant <155644357+jhavrlant@users.noreply.github.com>
2025-08-29 17:10:49 +0300

    upgrade_django-debug-toolbar (#3164)

--- webclient/webclient/urls.py
+++ webclient/webclient/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+
 from core.decorators import odstavka_in_progress
 from django.conf import settings
 from django.contrib import admin
@@ -91,7 +93,7 @@ urlpatterns += [re_path(r"^healthcheck/", include("healthcheck.urls"))]
 if "rosetta" in settings.INSTALLED_APPS:
     urlpatterns += [re_path(r"^rosetta/", include("rosetta.urls"))]
 
-if settings.DEBUG:
+if settings.DEBUG and not any("celery" in arg for arg in sys.argv):
     import debug_toolbar
 
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
