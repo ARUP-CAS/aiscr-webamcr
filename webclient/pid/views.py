ef385bbafab3376c62188ef768ac9562742ca673
Jiří Pešík <jiri.pesik@outlook.cz>
2025-09-14 22:30:56 +0200

    Fix value rendering #3125 (#3203)
    * Fix value rendering #3125
    
    * Returned filter for humans
    
    * Returned filter for humans

--- webclient/pid/views.py
+++ webclient/pid/views.py
@@ -218,13 +218,20 @@ class WikiDataAutocompleteView(LoginRequiredMixin, ApiView):
         else:
             q = unicodedata.normalize("NFKD", q)
             query = f"""
-                SELECT ?item ?itemLabel
+                SELECT DISTINCT ?item ?itemLabel
                 WHERE {{
-                  {{ ?item rdfs:label "{q}"@en }}
-                  UNION
-                  {{ ?item rdfs:label "{q}"@cs }}
+                  VALUES ?lang {{ "en" "cs" }}
+                  SERVICE wikibase:mwapi {{
+                    bd:serviceParam wikibase:endpoint "www.wikidata.org";
+                                   wikibase:api "EntitySearch";
+                                   mwapi:search "{q}";
+                                   mwapi:language ?lang;
+                                   wikibase:limit "50".
+                    ?item wikibase:apiOutputItem mwapi:item.
+                  }}
+                  ?item wdt:P31 wd:Q5. 
                   SERVICE wikibase:label {{
-                    bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,cs"
+                    bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,cs".
                   }}
                 }}
             """
@@ -240,7 +247,11 @@ class WikiDataAutocompleteView(LoginRequiredMixin, ApiView):
             id = result["item"]["value"]
             if "/" in id:
                 id = id.split("/")[-1]
-            result_list.append([id, f"{result['itemLabel']['value']} ({id})"])
+            if (title := result["itemLabel"]["value"]) and title != id and title != "Q":
+                title = f"{title} ({id})"
+            else:
+                title = id
+            result_list.append([id, title])
         return result_list
 
 
