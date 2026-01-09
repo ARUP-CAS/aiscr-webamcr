6c03a5b5fbb1d310052d8bad2eaf2a6d3cefb4a8
Jiří Pešík <jiri.pesik@outlook.cz>
2026-01-09 13:05:47 +0100

    Refactoring funkcí pro Django shell #3306 (#3409)
    * Refactoring funkcí pro Django shell #3306
    
    * Minor fixes #3306
    
    * Úpravy #3306
    
    * Code review #3306
    
    * Odstranění duplicity
    
    Remove duplicated sphinxcontrib-googleanalytics requirements.
    
    ---------
    
    Co-authored-by: David Novák <novak@arup.cas.cz>

---
+++ webclient/core/management/commands/check_pian_properties.py
@@ -0,0 +1,201 @@
+import logging
+
+from core.repository_connector import FedoraTransaction
+from django.contrib.gis.db.models.functions import Centroid
+from django.contrib.gis.geos import GeometryCollection, LineString, MultiPolygon, Point, Polygon
+from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Django management příkaz pro kontrolu a opravu vlastností PIANů.
+
+    Tento příkaz kontroluje a případně opravuje:
+
+    - Typ geometrie (typ) - musí odpovídat skutečnému typu geometrie (bod/linie/plocha)
+    - Základní mapy ZM10 a ZM50 - určí se podle pozice geometrie
+
+    Pro každý PIAN:
+
+    - Ověří, zda typ geometrie odpovídá skutečnosti
+    - Vypočítá reprezentativní bod geometrie (střed u linie, centroid u plochy)
+    - Určí příslušnost k základním mapám ZM10 a ZM50
+    - Pokud se některá hodnota liší, provede aktualizaci
+
+    Poznámka:
+        - Aktualizace jsou prováděny včetně Fedora transakcí a metadat
+        - Proces může trvat delší dobu v závislosti na počtu PIANů
+
+    Příklady použití::
+
+        python manage.py check_pian_properties
+    """
+
+    help = _("core.management.commands.check_pian_properties.Command.help")
+
+    def handle(self, *args, **options):
+        from heslar.hesla_dynamicka import GEOMETRY_BOD, GEOMETRY_LINIE, GEOMETRY_PLOCHA
+        from heslar.models import Heslar
+        from pian.models import Pian, get_ZM_from_point
+
+        logger.debug("core.management.commands.check_pian_properties.start")
+
+        # Prepare geometry type mapping
+        geom_type = {}
+        geom_type[str(Point)] = Heslar.objects.get(id=GEOMETRY_BOD)
+        geom_type[str(LineString)] = Heslar.objects.get(id=GEOMETRY_LINIE)
+        geom_type[str(Polygon)] = Heslar.objects.get(id=GEOMETRY_PLOCHA)
+        geom_type[str(MultiPolygon)] = Heslar.objects.get(id=GEOMETRY_PLOCHA)
+        geom_type[str(GeometryCollection)] = Heslar.objects.get(id=GEOMETRY_PLOCHA)
+
+        query = Pian.objects.all()
+        pocet_zmenenych = 0
+        pocet_pians = query.count()
+        index = 0
+
+        self.stdout.write(
+            _("core.management.commands.check_pian_properties.Command.handle.total_pian") + " " + str(pocet_pians)
+        )
+        self.stdout.write(_("core.management.commands.check_pian_properties.Command.handle.processing_pian"))
+        self.stdout.write("")
+
+        for item in query.iterator(chunk_size=1000):
+            save = False
+            changes = []
+            geom = item.geom
+
+            # Check geometry type
+            if item.typ.pk != geom_type[str(type(geom))].pk:
+                old_typ = str(item.typ)
+                item.typ = geom_type[str(type(geom))]
+                save = True
+                changes.append(
+                    _("core.management.commands.check_pian_properties.Command.handle.change_typ")
+                    + " "
+                    + old_typ
+                    + " -> "
+                    + str(item.typ)
+                )
+
+            # Calculate representative point based on geometry type
+            if isinstance(geom, Point):
+                point = geom
+            elif isinstance(geom, LineString):
+                point = geom.interpolate_normalized(0.5)
+            else:
+                point = Centroid(geom)
+
+            # Check ZM10 and ZM50
+            zm10, zm50 = get_ZM_from_point(point)
+            if zm10 is not None and zm50 is not None:
+                if item.zm10.pk != zm10.pk:
+                    old_zm10 = str(item.zm10)
+                    item.zm10 = zm10
+                    save = True
+                    changes.append(
+                        _("core.management.commands.check_pian_properties.Command.handle.change_zm10")
+                        + " "
+                        + old_zm10
+                        + " -> "
+                        + str(zm10)
+                    )
+                if item.zm50.pk != zm50.pk:
+                    old_zm50 = str(item.zm50)
+                    item.zm50 = zm50
+                    save = True
+                    changes.append(
+                        _("core.management.commands.check_pian_properties.Command.handle.change_zm50")
+                        + " "
+                        + old_zm50
+                        + " -> "
+                        + str(zm50)
+                    )
+
+            # Save if there were changes
+            if save is True:
+                pocet_zmenenych += 1
+                self.stdout.write(
+                    "\r"
+                    + _("core.management.commands.check_pian_properties.Command.handle.changed")
+                    + " "
+                    + str(pocet_zmenenych)
+                    + " | "
+                    + _("core.management.commands.check_pian_properties.Command.handle.processed")
+                    + " "
+                    + str(index)
+                    + "/"
+                    + str(pocet_pians)
+                    + " | "
+                    + _("core.management.commands.check_pian_properties.Command.handle.pian")
+                    + " "
+                    + str(item.pk)
+                    + ": "
+                    + ", ".join(changes),
+                    ending="",
+                )
+
+                fedora_transaction = FedoraTransaction()
+                item.active_transaction = fedora_transaction
+                item.update_all_azs = False
+                item.close_active_transaction_when_finished = True
+                item.save()
+
+            index += 1
+
+            # Show periodic progress even when no changes
+            if index % 100 == 0:
+                self.stdout.write(
+                    "\r"
+                    + _("core.management.commands.check_pian_properties.Command.handle.changed")
+                    + " "
+                    + str(pocet_zmenenych)
+                    + " | "
+                    + _("core.management.commands.check_pian_properties.Command.handle.processed")
+                    + " "
+                    + str(index)
+                    + "/"
+                    + str(pocet_pians),
+                    ending="",
+                )
+
+        self.stdout.write("")
+        self.stdout.write("")
+
+        logger.debug(
+            "core.management.commands.check_pian_properties.end",
+            extra={"total": pocet_pians, "changed": pocet_zmenenych},
+        )
+
+        self.stdout.write("=" * 50)
+        self.stdout.write(
+            _("core.management.commands.check_pian_properties.Command.handle.total_processed") + " " + str(pocet_pians)
+        )
+        self.stdout.write(
+            _("core.management.commands.check_pian_properties.Command.handle.changed_count")
+            + " "
+            + str(pocet_zmenenych)
+        )
+        self.stdout.write("=" * 50)
+        self.stdout.write("")
+
+        if pocet_zmenenych > 0:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    _("core.management.commands.check_pian_properties.Command.handle.finished_fixed")
+                    + " "
+                    + str(pocet_zmenenych)
+                    + " "
+                    + _("core.management.commands.check_pian_properties.Command.handle.of_total")
+                    + " "
+                    + str(pocet_pians)
+                )
+            )
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    _("core.management.commands.check_pian_properties.Command.handle.finished_no_changes")
+                )
+            )
--- webclient/core/management/commands/import_permissions.py
+++ webclient/core/management/commands/import_permissions.py
@@ -4,12 +4,32 @@ from core.exceptions import WrongCSVError
 from core.services import PermissionService
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management.base import BaseCommand
+from django.utils.translation import gettext as _
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Import oprávnění z core/resources/uzivatelska_prava.csv"
+    """
+    Django management příkaz pro import uživatelských oprávnění z CSV souboru.
+
+    Tento příkaz načte CSV soubor ``core/resources/uzivatelska_prava.csv``
+    a importuje uživatelská oprávnění do databáze pomocí PermissionService.
+
+    CSV soubor obsahuje definice uživatelských práv a jejich přiřazení.
+    Při importu se kontroluje správnost formátu a hodnot.
+
+    Poznámka:
+        - CSV soubor musí být umístěn v adresáři ``core/resources/``
+        - Při chybě ve formátu CSV se import přeruší a zobrazí se chybová hláška
+        - Úspěšný import zobrazí počet importovaných oprávnění a případné chybějící hodnoty
+
+    Příklady použití::
+
+        python manage.py import_permissions
+    """
+
+    help = _("core.management.commands.import_permissions.Command.help")
 
     def handle(self, *args, **options):
         logger.debug("core.management.commands.import_permissions.start")
@@ -26,11 +46,15 @@ class Command(BaseCommand):
                 "core.management.commands.import_permissions.WrongCSVError",
                 extra={"error": err},
             )
-            self.stdout.write(self.style.WARNING("Import oprávnění skončil chybou"))
+            self.stdout.write(
+                self.style.WARNING(_("core.management.commands.import_permissions.Command.handle.finished_error"))
+            )
         else:
             logger.info(
                 "core.management.commands.import_permissions.results",
                 extra={"data": sheet.to_string(), "missing": str(missing)},
             )
-            self.stdout.write(self.style.SUCCESS("Import oprávnění OK"))
+            self.stdout.write(
+                self.style.SUCCESS(_("core.management.commands.import_permissions.Command.handle.finished_success"))
+            )
         logger.debug("core.management.commands.import_permissions.end")
