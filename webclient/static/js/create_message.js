53dbcbd6316dea43d52de2e0a9a04d8451d8c304
David Novák <novak@arup.cas.cz>
2026-02-28 00:45:07 +0100

    Update create_message.js

--- webclient/static/js/create_message.js
+++ webclient/static/js/create_message.js
@@ -1,22 +1,25 @@
 function createMessage(tag, message, button = 'False', append = 'True', alert = 'True') {
+    let msg;
     if (button === 'True') {
         msg = `<div class="alert alert-${tag} alert-dismissible fade show app-alert-floating" role="alert">
         <button id="prolong_btn" type="button" class="btn btn-prolong">${message}</button>
-        <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></div>`;
+        <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></button></div>`;
     } else {
         msg = `<div class="alert alert-${tag} alert-dismissible fade show app-alert-floating" role="alert">
             ${message}
-            <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></div>`;
+            <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></button></div>`;
     }
 
     if (append === 'True') {
-        $('.message-container').append(msg);
+        const $msg = $(msg);
+        $('.message-container').append($msg);
+    
         if (alert === 'True') {
-            $(".app-alert-floating").delay(5000).slideUp(200, function () {
+            $msg.delay(5000).slideUp(200, function () {
                 $(this).alert('close');
             });
         }
     } else {
-        return msg
+        return msg;
     }
 }
