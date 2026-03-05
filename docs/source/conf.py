7fbb1f702dbc13f7c602581dc9d6e42cfef4af31
David Novák <novak@arup.cas.cz>
2026-03-05 16:26:46 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,8 +39,8 @@ exclude_patterns = []
 
 language = "cs"
 
-# -- Nastavení HTML výstupu --------------------------------------------------
-
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
