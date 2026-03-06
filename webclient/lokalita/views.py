67461e25981c0602d6fc7b50e55943862bc8b053
David Novák <novak@arup.cas.cz>
2026-03-06 21:27:58 +0100

    Update views.py

--- webclient/lokalita/views.py
+++ webclient/lokalita/views.py
@@ -668,7 +668,6 @@ class LokalitaPianCreateView(LokalitaDokumentacniJednotkaRelatedView):
         if "index" in self.request.GET and "label" in self.request.GET:
             try:
                 geom = cache.get(str(request.user.id) + "_geom")
-                # Mazání cache geometrie je zde záměrně zakomentované.
                 index = int(self.request.GET["index"])
                 if self.request.GET["label"] != str(geom.iloc[index]["label"]):
                     raise Exception("lokalita.views.LokalitaPianCreateView.get.label_not_found")
@@ -759,7 +758,6 @@ class LokalitaPianUpdateView(LokalitaDokumentacniJednotkaRelatedView):
         if "index" in self.request.GET and "label" in self.request.GET:
             try:
                 geom = cache.get(str(request.user.id) + "_geom")
-                # Mazání cache geometrie je zde záměrně zakomentované.
                 index = int(self.request.GET["index"])
                 if self.request.GET["label"] != str(geom.iloc[index]["label"]):
                     raise Exception("lokalita.views.LokalitaPianUpdateView.get.label_not_found")
