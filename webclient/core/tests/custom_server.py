9da574467ce106776f5c1f866ad43428f66a443b
Jan Havrlant <jhavrlant@arup.cas.cz>
2026-04-10 09:49:57 +0200

    Oprava testů

--- webclient/core/tests/custom_server.py
+++ webclient/core/tests/custom_server.py
@@ -1,10 +1,11 @@
 import logging
 import socket
+import ssl
 from threading import Event, Thread
 
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
-from werkzeug.serving import run_simple
+from werkzeug.serving import make_ssl_devcert, run_simple
 
 
 class WerkzeugServerThread(Thread):
@@ -30,6 +31,21 @@ class WerkzeugServerThread(Thread):
         Nainstaluje SSL certifikáty a klíče pro HTTPS testovacího serveru.
 
         :return: Cesta k certifikátu.
+        """
+        try:
+            cert_path, key_path = make_ssl_devcert("./core/tests/resources/ssl", host="localhost")
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+            self.ssl_context = context
+        except Exception as e:
+            self.error = str(e)
+
+    def run(self):
+        """
+        Spustí Werkzeug testovací server ve vlákně.
+
+        Načte SSL kontext, inicializuje WSGI aplikaci a spustí server.
+        Nastaví ``is_ready`` event po úspěšném spuštění nebo uloží chybu při selhání.
         """
         try:
             self.setup_ssl()
