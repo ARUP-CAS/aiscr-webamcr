0d403a16f36271b535ac301586b3d2a34c1b0ac5
Jan Havrlant <jhavrlant@arup.cas.cz>
2026-04-16 16:31:55 +0200

    Zachycení vyjímky NelzeZjistitRaduError při vytvoření dokumentu

--- webclient/core/message_constants.py
+++ webclient/core/message_constants.py
@@ -126,6 +126,9 @@ DOKUMENT_SE_NEPODARILO_VRATIT: Final = _(
 )  # Dokument se nepodařilo vrátit.
 DOKUMENT_NELZE_ODESLAT: Final = _("common.message.DOKUMENT_NELZE_ODESLAT.text")  # Dokument nelze odeslat.
 DOKUMENT_NELZE_ARCHIVOVAT: Final = _("common.message.DOKUMENT_NELZE_ARCHIVOVAT.text")  # Dokument nelze archivovat.
+DOKUMENT_NELZE_VYTVORIT_RADA: Final = _(
+    "core.message_constants.dokumentNelzeVytvoriteRada.text"
+)  # Dokument nelze vytvořit: nelze určit řadu dokumentu pro zadanou kombinaci typu a materiálu.
 DOKUMENT_NELZE_ARCHIVOVAT_CHYBY_SOUBOR: Final = _("core.message_constants.dokumentNelzeArchivovatChybaSouboru.text")
 DOKUMENT_USPESNE_PRIPOJEN: Final = _("common.message.DOKUMENT_USPESNE_PRIPOJEN.text")  # Dokument úspěšně připojen.
 DOKUMENT_JIZ_BYL_PRIPOJEN: Final = _("common.message.DOKUMENT_JIZ_BYL_PRIPOJEN.text")  # Dokument již byl připojen!
