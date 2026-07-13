4831319ab6007d6399a8f9aaa7d353f55518e257
Jiří Pešík <jiri.pesik@outlook.cz>
2026-07-13 21:26:26 +0200

    IGSN fix #3196

--- webclient/api/views.py
+++ webclient/api/views.py
@@ -3442,8 +3442,8 @@ class SamostatnyNalezFotografieUploadView(PasApiBaseView):
                 soubor_instance.save()
                 soubor_instance.zaznamenej_nahrani(request.user, uploaded_file.name)
                 self._create_rearchive_history_record(instance, request.user)
-                self._update_igsn_if_archived(instance)
                 soubor_instance.save()
+            self._update_igsn_if_archived(instance)
         except DoiWriteError as err:
             logger.error("api.views.SamostatnyNalezFotografieUploadView.post.igsn_error", extra={"error": err})
             if fedora_transaction.status == FedoraTransactionStatus.ACTIVE:
